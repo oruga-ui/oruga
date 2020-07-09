f98682015b46fe0f5aaa865c01be20b7e167c06d
Walter Tommasi <tommsi20@gmail.com>
2020-07-09 17:28:30 +0200

    Improve meta tags

--- packages/docs/.vuepress/config.js
+++ packages/docs/.vuepress/config.js
@@ -6,9 +6,25 @@ const components = fs.readdirSync(componentsFolder).filter(c => c !== 'README.md
 
 module.exports = {
   title: 'Oruga',
-  description: 'Documentations and API for Oruga',
+  description: 'UI components for Vue.js and CSS framework agnostic',
   head: [
-    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    ['meta', { charset: 'utf-8' }],
+    ['meta', { name: 'viewport', content: 'width=device-width, initial-scale=1' }],
+    ['meta', { title: 'Oruga UI - Vue.js' }],
+    ['meta', { name: 'og:locale', content: 'en_US' }],
+    ['meta', { name: 'og:type', content: 'website' }],
+    ['meta', { name: 'og:url', content: 'https://oruga.io' }],
+    ['meta', { name: 'og:title', content: 'UI components for Vue.js and CSS framework agnostic' }],
+    ['meta', { name: 'og:site_name', content: 'Oruga' }],
+    ['meta', { name: 'og:description', content: 'Oruga UI is like a caterpillar, minimal and yet functional. It\'s in your hands turning it into a butterfly' }],
+    ['meta', { name: 'og:image', content: 'https://oruga.io/logo.png' }],
+    ['meta', { name: 'og:image:type', content: 'image/png' }],
+    ['meta', { name: 'twitter:card', content: 'summary_large_image' }],
+    ['meta', { name: 'twitter:site', content: '@walter_tommasi' }],
+    ['meta', { name: 'twitter:title', content: 'UI components for Vue.js and CSS framework agnostic' }],
+    ['meta', { name: 'twitter:description', content: 'Oruga UI is like a caterpillar, minimal and yet functional. It\'s in your hands turning it into a butterfly' }],
+    ['meta', { name: 'twitter:image', content: 'https://oruga.io/logo.png' }]
   ],
   plugins: [
     [
