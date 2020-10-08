e09d80de65e2faecce68d6f60e7621dda1935aa0
Walter Tommasi <tommsi20@gmail.com>
2020-10-08 22:54:05 +0200

    Remove process.env in vue-utils

--- packages/oruga/src/utils/vue-utils.js
+++ packages/oruga/src/utils/vue-utils.js
@@ -2,8 +2,7 @@ import { VueInstance } from './config'
 
 export const isVue2 = (instance) => {
     const vm = instance || VueInstance
-    if (process && process.env.NODE_ENV === 'test') return true
-    return vm && vm.version.indexOf('2.') === 0
+    return !vm || (vm && vm.version.indexOf('2.') === 0)
 }
 
 export const use = (vm, plugin) => {
