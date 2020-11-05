76de37644835cdce718cd1be4e64bb77cd9dc406
Walter Tommasi <tommsi20@gmail.com>
2020-11-06 00:50:59 +0100

    Fix helpers

--- packages/oruga/src/utils/helpers.js
+++ packages/oruga/src/utils/helpers.js
@@ -213,7 +213,7 @@ export function getMonthNames(locale = undefined, format = 'long') {
     }
     const dtf = new Intl.DateTimeFormat(locale, {
         month: format,
-        timezome: 'UTC'
+        timeZone: 'UTC'
     })
     return dates.map((d) => dtf.format(d))
 }
@@ -237,7 +237,7 @@ export function getWeekdayNames(locale = undefined, firstDayOfWeek = 0, format =
     }
     const dtf = new Intl.DateTimeFormat(locale, {
         weekday: format,
-        timezome: 'UTC'
+        timeZone: 'UTC'
     })
     return dates.map((d) => dtf.format(d))
 }
