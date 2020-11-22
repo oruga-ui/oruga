217d5fbe1cada280f032d23fea07503e2f74719e
Walter Tommasi <tommsi20@gmail.com>
2020-11-22 18:19:00 +0100

    Remove oruga-tailwind package

--- packages/oruga-tailwindcss/postcss.config.js
+++
@@ -1,9 +0,0 @@
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss')('./tailwind.config.js');
-
-module.exports = {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ]
-};
