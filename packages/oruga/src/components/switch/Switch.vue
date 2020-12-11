9c0d9877b75fcd1034ae78f82f472dd19ba439db
Andrea Stagi <stagi.andrea@gmail.com>
2020-12-11 13:47:18 +0100

    First attempt to refactor Switch using BEM

--- packages/oruga/src/components/switch/Switch.vue
+++ packages/oruga/src/components/switch/Switch.vue
@@ -115,24 +115,25 @@ export default {
         rootClasses() {
             return [
                 this.computedClass('rootClass', 'o-switch'),
-                { [this.computedClass('sizeClass', 'o-size-', this.size)]: this.size },
-                { [this.computedClass('disabledClass', 'o-switch-disabled')]: this.disabled },
-                { [this.computedClass('roundedClass', 'o-switch-rounded')]: this.rounded },
-                { [this.computedClass('outlinedClass', 'o-switch-outlined')]: this.outlined },
-                { [this.computedClass('leftLabelClass', 'o-switch-left')]: this.leftLabel }
+                { [this.computedClass('sizeClass', 'o-switch--', this.size)]: this.size },
+                { [this.computedClass('disabledClass', 'o-switch--disabled')]: this.disabled },
+                { [this.computedClass('roundedClass', 'o-switch--rounded')]: this.rounded },
+                { [this.computedClass('outlinedClass', 'o-switch--outlined')]: this.outlined },
+                { [this.computedClass('leftLabelClass', 'o-switch--left')]: this.leftLabel }
             ]
         },
         checkClasses() {
             return [
-                this.computedClass('checkClass', 'o-switch-check'),
-                { [this.computedClass('animationClass', 'o-switch-elastic')]: (this.isMouseDown && !this.disabled)},
-                { [this.computedClass('variantClass', 'o-color-', this.variant)]: this.variant },
-                { [this.computedClass('passiveVariantClass', 'o-color-', this.passiveVariant + '-passive')]: this.passiveVariant }
+                this.computedClass('checkClass', 'o-switch__check'),
+                { [this.computedClass('animationClass', 'o-switch__check--checked')]: (this.newValue !== this.falseValue)},
+                { [this.computedClass('animationClass', 'o-switch__check--elastic')]: (this.isMouseDown && !this.disabled)},
+                { [this.computedClass('variantClass', 'o-switch__check--', this.variant)]: this.variant },
+                { [this.computedClass('passiveVariantClass', 'o-switch__check--', this.passiveVariant + '-passive')]: this.passiveVariant }
             ]
         },
         labelClasses() {
             return [
-                this.computedClass('labelClass', 'o-switch-label')
+                this.computedClass('labelClass', 'o-switch__label')
             ]
         },
         computedValue: {
