2ab6f6fd09be13eedefce9d4768b79e6ba95b72a
Andrea Stagi <stagi.andrea@gmail.com>
2021-01-04 17:32:54 +0100

    Add Button inspector

---
+++ packages/oruga/src/components/button/Inspector.vue
@@ -0,0 +1,83 @@
+<template>
+  <div>
+    <doc-wrapper>
+      <template v-slot:default="s">
+      <o-button v-bind="s">Button</o-button>
+      </template>
+    </doc-wrapper>
+    <inspector :inspectData="inspectData"></inspector>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            inspectData: [
+                {
+                    class: "rootClass",
+                    description: "",
+                },
+                {
+                    class: "outlinedClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data.variant = 'primary';
+                        cmp.data.outlined = true;
+                    },
+                },
+                {
+                    class: "invertedClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data.variant = 'primary';
+                        cmp.data.inverted = true;
+                    },
+                },
+                {
+                    class: "expandedClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data.expanded = true;
+                    },
+                },
+                {
+                    class: "roundedClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data.rounded = true;
+                    },
+                },
+                {
+                    class: "disabledClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data.disabled = true;
+                    },
+                },
+                {
+                    class: "iconClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data['icon-left'] = "plus"
+                    },
+                },
+                {
+                    class: "sizeClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data.size = 'large';
+                    },
+                },
+                {
+                    class: "variantClass",
+                    description: "",
+                    action: (cmp) => {
+                        cmp.data.variant = 'warning';
+                    },
+                },
+            ],
+        };
+    }
+}
+</script>
