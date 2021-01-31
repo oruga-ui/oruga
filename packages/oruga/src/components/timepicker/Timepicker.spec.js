b731b90300e7902be8de153b01f00767fbc395de
Andrea Stagi <stagi.andrea@gmail.com>
2021-02-01 00:46:51 +0100

    Try fix tests

--- packages/oruga/src/components/timepicker/Timepicker.spec.js
+++ packages/oruga/src/components/timepicker/Timepicker.spec.js
@@ -17,23 +17,27 @@ describe('Timepicker', () => {
         expect(wrapper.html()).toMatchSnapshot()
     })
 
+    // https://github.com/vuejs/vue-test-utils/issues/738#issuecomment-674988368
+    it('returns "1" for nativeStep if enableSeconds is true', async () => {
+        expect(wrapper.vm.nativeStep).toBeNull()
+        wrapper = shallowMount(
+            OTimepicker, {
+                propsData: { enableSeconds: true }
+            }
+        )
+        await wrapper.vm.$nextTick()
+        expect(wrapper.vm.nativeStep).toBe('1')
+    })
 
-    // it('returns "1" for nativeStep if enableSeconds is true', async () => {
-    //     expect(wrapper.vm.nativeStep).toBeNull()
-    //     wrapper.setProps({ enableSeconds: true })
-    //     await wrapper.vm.$nextTick()
-    //     expect(wrapper.vm.nativeStep).toBe('1')
-    // })
 
-
-    // it('disables hour if no selectable minutes available', async () => {
-    //     wrapper.setProps({
-    //         minTime: new Date(2019, 8, 22, 11, 55),
-    //         incrementMinutes: 10
-    //     })
-    //     await wrapper.vm.$nextTick()
-    //     expect(wrapper.find('option[value="11"]').attributes()['disabled']).toBe('disabled')
-    //     expect(wrapper.find('option[value="12"]').attributes()['disabled']).not.toBe('disabled')
-    // })
+    it('disables hour if no selectable minutes available', async () => {
+        wrapper.setProps({
+            minTime: new Date(2019, 8, 22, 11, 55),
+            incrementMinutes: 10
+        })
+        await wrapper.vm.$nextTick()
+        expect(wrapper.find('option[value="11"]').attributes()['disabled']).toBe('disabled')
+        expect(wrapper.find('option[value="12"]').attributes()['disabled']).not.toBe('disabled')
+    })
 
 })
