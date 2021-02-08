4949e7cb05aafa406f751a8dc0fa5632d5676e55
Andrea Stagi <stagi.andrea@gmail.com>
2021-02-08 23:36:22 +0100

    Fix css switch when you remove styles

--- packages/docs/.vuepress/theme/components/Navbar.vue
+++ packages/docs/.vuepress/theme/components/Navbar.vue
@@ -95,25 +95,29 @@ export default {
             // switch style
             if (this.lite) {
                 let style = document.getElementById('orugacss-lite')
-                if (style) document.head.removeChild(document.getElementById('orugacss'))
+                let styleToRemove = document.getElementById('orugacss')
                 style = document.createElement('link')
                 style.id = 'orugacss-lite'
                 style.type = "text/css"
                 style.rel = "stylesheet"
                 style.href = `${location.origin}/cdn/oruga.min.css`
                 document.head.appendChild(style)
+                setTimeout(() => {
+                    if (styleToRemove) document.head.removeChild(styleToRemove)
+                }, 100);
             } else {
                 let style = document.getElementById('orugacss')
-                if (style) document.head.removeChild(document.getElementById('orugacss-lite'))
+                let styleToRemove = document.getElementById('orugacss-lite')
                 style = document.createElement('link')
                 style.id = 'orugacss'
                 style.type = "text/css"
                 style.rel = "stylesheet"
                 style.href = `${location.origin}/cdn/oruga-full-vars.min.css`
                 document.head.appendChild(style)
+                setTimeout(() => {
+                    if (styleToRemove) document.head.removeChild(styleToRemove)
+                }, 100);
             }
-            // style.href = this.lite ? `https://oruga.io/cdn/oruga.min.css` : `https://oruga.io/cdn/oruga-full-vars.min.css`
-
         }, immediate ? 0 : 250)
     }
   },
