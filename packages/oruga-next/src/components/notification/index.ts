25aa25c321d9b653f0745002b63cd0c947f8da12
Andrea Stagi <stagi.andrea@gmail.com>
2021-05-11 21:28:06 +0200

    feat: add vue3 support for notification

---
+++ packages/oruga-next/src/components/notification/index.ts
@@ -0,0 +1,77 @@
+import Notification from './Notification.vue'
+import NotificationNotice from './NotificationNotice.vue'
+
+import { getOptions } from '../../utils/config'
+import { getValueByPath } from '../../utils/helpers'
+import { merge } from '../../utils/helpers'
+import { VueInstance } from '../../utils/config'
+import { registerComponent, registerComponentProgrammatic } from '../../utils/plugins'
+
+import { App, createVNode, DefineComponent, Plugin, render } from 'vue'
+
+
+let localVueInstance: App
+
+const NotificationProgrammatic = {
+    open(params) {
+        let parent
+        let newParams
+        if (typeof params === 'string') {
+            newParams = {
+                message: params
+            }
+        } else {
+            newParams = params
+        }
+
+        const defaultParam = {
+            programmatic: true,
+            position: getValueByPath(getOptions(), 'notification.position', 'top-right'),
+            closable: params.closable || getValueByPath(getOptions(), 'notification.closable', false)
+        }
+        if (newParams.parent) {
+            parent = params.parent
+            delete params.parent
+        }
+        let slot
+        if (Array.isArray(newParams.message)) {
+            slot = newParams.message
+            delete newParams.message
+        }
+
+        newParams.active = true
+        const propsData = merge(defaultParam, newParams)
+
+        const app = localVueInstance || VueInstance
+        propsData.propsNotification = Object.assign({},propsData)
+        propsData.propsNotification.isActive = true
+        const defaultSlot = () => { return newParams.message }
+        const vnode = createVNode(NotificationNotice, propsData, defaultSlot)
+        vnode.appContext = app._context
+
+        // if (slot) {
+        //     vnode.component.slots.default = slot
+        //     //vnode.dynamicChildren[0].$forceUpdate()
+        // }
+        // vnode.component.props.isActive = true
+        return render(vnode, document.createElement('div'))
+    }
+}
+
+export default {
+    install(app: App) {
+        localVueInstance = app
+        registerComponent(app, Notification)
+        registerComponentProgrammatic(app, 'notification', NotificationProgrammatic)
+    }
+} as Plugin
+
+export {
+    Notification as ONotification,
+    NotificationProgrammatic,
+}
+
+export interface ONotification {
+    parent: any | undefined;
+    content: string | DefineComponent[] | undefined;
+}
