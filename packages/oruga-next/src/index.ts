be8665b829dc349efec9a61b893f32fafc082346
David Christmann <chd@christmann.info>
2021-07-08 22:42:47 +0200

    feat: provide programmatic usage

--- packages/oruga-next/src/index.ts
+++ packages/oruga-next/src/index.ts
@@ -18,6 +18,8 @@ const Oruga = {
         }
         // Config component
         registerComponentProgrammatic(app, 'config', ConfigProgrammatic)
+        // Provide programmatic usage
+        app.provide('oruga', app.config.globalProperties.$oruga);
     }
 }
 
