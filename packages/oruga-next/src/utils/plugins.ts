fef7bd4c93757ce201b01e4d9b5cc10a95be0b57
cimchd <49520229+cimchd@users.noreply.github.com>
2021-08-27 17:06:33 +0200

    feat: add provide programmatic usage (oruga-next)

--- packages/oruga-next/src/utils/plugins.ts
+++ packages/oruga-next/src/utils/plugins.ts
@@ -1,5 +1,6 @@
 
 import { App, DefineComponent, Plugin } from 'vue'
+import { useProgrammatic } from './useProgrammatic'
 
 export const registerPlugin = (app: App, plugin: Plugin) => {
     app.use(plugin)
@@ -10,6 +11,13 @@ export const registerComponent = (app: App, component: DefineComponent) => {
 }
 
 export const registerComponentProgrammatic = (app: App, property: string, component: any) => {
-    if (!app.config.globalProperties.$oruga) app.config.globalProperties.$oruga = {}
-    app.config.globalProperties.$oruga[property] = component
-}
+    // use composable for unified access to programmatic oruga object
+    const { oruga, addProgrammatic } = useProgrammatic();
+
+    // add component (manipulates the programmatic oruga object)
+    addProgrammatic(property, component);
+
+    // add provide and $oruga (only needed once)
+    if (!(app._context.provides && app._context.provides.oruga)) app.provide('oruga', oruga);
+    if (!app.config.globalProperties.$oruga) app.config.globalProperties.$oruga = oruga;
+  }
