9456979741029154325865b69a3b0f5c140a2de9
Walter Tommasi <tommsi20@gmail.com>
2021-10-16 17:25:13 +0200

    fix: reset input value when autocomplete clear-on-select

--- packages/oruga-next/src/components/autocomplete/Autocomplete.vue
+++ packages/oruga-next/src/components/autocomplete/Autocomplete.vue
@@ -458,8 +458,9 @@ export default defineComponent({
             this.$emit('select', this.selected, event)
             if (this.selected !== null) {
                 if (this.clearOnSelect) {
-                    const input = this.$refs.input.$refs.input
-                    input.value = ''
+                    const input = this.$refs.input
+                    input.newValue = ''
+                    input.$refs.input.value = ''
                 } else {
                     this.newValue = this.getValue(this.selected)
                 }
