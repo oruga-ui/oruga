0fb6a34ba4e63c4bea25c62677f463abb4220488
Mouad Taoussi <mouadtaoussi0@gmail.com>
2021-10-17 13:59:27 +0100

    adding the type definitions for Notification;Modal;Loading

---
+++ packages/oruga/types/components/Loading.d.ts
@@ -0,0 +1,61 @@
+import * from '../helpers'
+
+export interface Loading {
+    active?: boolean;
+    programmatic?: boolean;
+    container?: any ;  ///////////////
+    fullPage?: boolean;
+    animation?: string;
+    canCancel?: boolean;
+    onCancel?: any; /////////////
+    icon?: string;
+    iconSpin?: boolean;
+    iconSize?: string; ////////////////
+    overlay?: boolean;
+}
+
+ /** Whether modal is active or not,  use the .sync modifier (Vue 2.x) or v-model:active (Vue 3.x) to make it two-way binding */
+active: Boolean,
+programmatic: Boolean,
+container: [Object, Function, HTMLElement],
+/** Loader will overlay the full page */
+fullPage: {
+    type: Boolean,
+    default: true
+},
+/* Custom animation (transition name) */
+animation: {
+    type: String,
+    default: () => { return getValueByPath(getOptions(), 'loading.animation', 'fade') }
+},
+/** Can close Loading by pressing escape or clicking outside */
+canCancel: {
+    type: Boolean,
+    default: false
+},
+/** Callback function to call after user canceled (pressed escape / clicked outside) */
+onCancel: {
+    type: Function,
+    default: () => {}
+},
+/** Icon name */
+icon: {
+    type: String,
+    default: () => { return getValueByPath(getOptions(), 'loading.icon', 'loading') }
+},
+/** Enable spin effect on icon */
+iconSpin: {
+    type: Boolean,
+    default: true
+},
+iconSize: {
+    type: String,
+    default: 'medium'
+},
+overlay: {
+    type: Boolean,
+    default: true
+},
+rootClass: [String, Function, Array],
+overlayClass: [String, Function, Array],
+iconClass: [String, Function, Array]
---
+++ packages/oruga/types/components/Notification.d.ts
@@ -0,0 +1,62 @@
+import * from '../helpers'
+
+export interface Modal {
+    position?: string;
+    variant?: string | object; ////////////////
+    ariaCloseLabel?: string;
+    animation?: string;
+    component?: object | Function; ////////////
+    props?: object;
+    events?: object;
+    closeIcon?: string;
+    rootClass?: any; ///////////////
+    closeClass?: any; ///////////////
+    contentClass?: any; ///////////////
+    iconClass?: any; ///////////////
+    positionClass?: any; ///////////////
+    variantClass?: any; ///////////////
+    wrapperClass?: any; ///////////////
+    
+}
+
+ /**
+* Which position the notification will appear when programmatically
+* @values top-right, top, top-left, bottom-right, bottom, bottom-left
+*/
+position: String,
+/**
+* Color of the control, optional
+* @values primary, info, success, warning, danger, and any other custom color
+*/
+variant: [String, Object],
+/**
+ * Label for the close button, to be read by accessibility screenreaders.
+ */
+ariaCloseLabel: String,
+/**
+ * Custom animation (transition name).
+ */
+animation: {
+    type: String,
+    default: 'fade'
+},
+/** Component to be injected, used to open a component modal programmatically. Close modal within the component by emitting a 'close' event — this.$emit('close') */
+component: [Object, Function],
+/** Props to be binded to the injected component */
+props: Object,
+/** Events to be binded to the injected component */
+events: Object,
+/** Close icon name */
+closeIcon: {
+    type: String,
+    default: () => {
+        return getValueByPath(getOptions(), 'notification.closeIcon', 'times')
+    }
+},
+rootClass: [String, Function, Array],
+closeClass: [String, Function, Array],
+contentClass: [String, Function, Array],
+iconClass: [String, Function, Array],
+positionClass: [String, Function, Array],
+variantClass: [String, Function, Array],
+wrapperClass: [String, Function, Array],
