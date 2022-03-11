321d18588727e7701dd2844d7f732a347cbd0c07
Andrea Stagi <stagi.andrea@gmail.com>
2022-03-11 01:25:36 +0100

    feat: add instrumentation

--- packages/oruga-next/cypress/plugins/index.js
+++ packages/oruga-next/cypress/plugins/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const { startDevServer } = require('@cypress/vite-dev-server')
 
 module.exports = (on, config) => {
+  require('@cypress/code-coverage/task')(on, config)
   on('dev-server:start', (options) => {
     return startDevServer({
       options,
--- packages/oruga-next/vite.config.js
+++ packages/oruga-next/vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+      vue(),
+      istanbul({
+        include: 'src/*',
+        exclude: ["node_modules", "cypress/"],
+        extension: [".ts", ".vue"],
+      })
+  ]
 })
