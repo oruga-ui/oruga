ee54079525933507a0b413468c997ecb81973c22
Egor Popov <uporot1k@yandex.ru>
2022-04-22 22:20:59 +0300

    rewrite ExampleViewer & add showCode prop

--- packages/docs-next/.vuepress/theme/components/ExampleViewer.vue
+++ packages/docs-next/.vuepress/theme/components/ExampleViewer.vue
@@ -1,66 +1,76 @@
 <template>
-    <div class="odocs-mt">
-        <component :is="cmp"></component>
-    </div>
-    <div class="odocs-mt">
-        <o-collapse
-            root-class="odocs-panel"
-            trigger-class="odocs-panel-trigger"
-            :open="open">
-            <template #trigger>Show code</template>
-            <div class="language-vue ext-vue">
-                <pre class="language-vue">
-                    <code class="odocs-code">{{ code }}</code>
-                </pre>
-            </div>
-        </o-collapse>
-    </div>
+  <div class="odocs-mt">
+    <component :is="cmp"></component>
+  </div>
+  <div v-if="showCode" class="odocs-mt">
+    <o-collapse root-class="odocs-panel" trigger-class="odocs-panel-trigger" :open="isOpen">
+      <template #trigger>Show code</template>
+      <div class="language-vue ext-vue">
+        <pre class="language-vue">
+            <code class="odocs-code">{{ code }}</code>
+        </pre>
+      </div>
+    </o-collapse>
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, markRaw } from 'vue'
+import { defineComponent, ref, markRaw, onBeforeMount } from 'vue'
 
 export default defineComponent({
-    props: {
-        example: String
+  props: {
+    example: {
+      required: true,
+      type: String,
     },
-    data() {
-        return {
-            open: false,
-            cmp: undefined,
-            code: ''
-        }
+    showCode: {
+      type: Boolean,
+      default: () => true,
     },
-    beforeCreate() {
-        import(`../../examples/${this.example}.vue`).then(val => {
-            this.cmp = markRaw(val.default)
-        })
+  },
+  setup(props) {
+    const isOpen = ref(false);
+    const cmp = ref(null);
+    const code = ref('');
 
-        import(`../../examples/${this.example}.vue?raw`).then(val => {
-            this.code = val.default
-        })
-    }
+    onBeforeMount(() => {
+      import(`../../examples/${props.example}.vue`).then(val => {
+        cmp.value = markRaw(val.default);
+      })
+
+      import(`../../examples/${props.example}.vue?raw`).then(val => {
+        code.value = val.default
+      })
+    });
+
+    return {
+      code,
+      cmp,
+      isOpen,
+    };
+  },
 })
 </script>
 
 <style lang="scss">
 .odocs-mt {
-    margin-top: 1rem;
+  margin-top: 1rem;
 }
-.odocs-spaced > *:not(:last-child) {
-    margin-right: 0.5rem;
-    margin-bottom: 0.5rem;
+
+.odocs-spaced>*:not(:last-child) {
+  margin-right: 0.5rem;
+  margin-bottom: 0.5rem;
 }
 
 .odocs-panel-trigger {
-    cursor: pointer;
-    text-align: center;
-    padding: 0.5rem;
-    border: 1px solid #ebebeb;
+  cursor: pointer;
+  text-align: center;
+  padding: 0.5rem;
+  border: 1px solid #ebebeb;
 }
 
 .odocs-code {
-    display: flex;
-    width: 100%;
+  display: flex;
+  width: 100%;
 }
 </style>
