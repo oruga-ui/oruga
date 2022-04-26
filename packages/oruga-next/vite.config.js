e9b5d695fbb2d1d334470f17cd63a447c1b7d3d8
Andrea Stagi <stagi.andrea@gmail.com>
2022-04-26 08:39:37 +0200

    chore: code formatting

--- packages/oruga-next/vite.config.js
+++ packages/oruga-next/vite.config.js
@@ -4,13 +4,13 @@ import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-      vue(),
-      istanbul({
-        include: 'src/*',
-        exclude: ["node_modules", "cypress/"],
-        extension: [".ts", ".vue"],
-        cypress: true
-      })
-  ]
+    plugins: [
+        vue(),
+        istanbul({
+            include: 'src/*',
+            exclude: ["node_modules", "cypress/", "**/tests/*"],
+            extension: [".ts", ".vue"],
+            cypress: true
+        })
+    ]
 })
