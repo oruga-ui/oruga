0c9aec1572d663af65b3dfbf94c2b0a981c8058e
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2022-08-07 12:41:59 +0900

    feat(oruga, oruga-next): Export individual components
    It allows components to be registered locally instead of globally (see [1]).
    Some IDE can automatically insert these imports which greatly reduces the hassle
    of using tree shaking.
    
    It doesn't handle CSS imports yet, but the old implementation did not either.
    A promising solution is to use a bundler plugin, which is adopted by Element
    Plus [2].
    
    [1]: https://vuejs.org/guide/components/registration.html#local-registration
    [2]: https://github.com/element-plus/unplugin-element-plus

---
+++ packages/oruga-next/src/components/plugins.ts
@@ -0,0 +1,59 @@
+import Autocomplete from './autocomplete'
+import Button from './button'
+import Carousel from './carousel'
+import Checkbox from './checkbox'
+import Collapse from './collapse'
+import Datepicker from './datepicker'
+import Datetimepicker from './datetimepicker'
+import Dropdown from './dropdown'
+import Field from './field'
+import Icon from './icon'
+import Input from './input'
+import Inputitems from './inputitems'
+import Loading from './loading'
+import Modal from './modal'
+import Notification from './notification'
+import Pagination from './pagination'
+import Radio from './radio'
+import Select from './select'
+import Skeleton from './skeleton'
+import Sidebar from './sidebar'
+import Slider from './slider'
+import Steps from './steps'
+import Switch from './switch'
+import Table from './table'
+import Tabs from './tabs'
+import Timepicker from './timepicker'
+import Tooltip from './tooltip'
+import Upload from './upload'
+
+export {
+    Autocomplete,
+    Button,
+    Carousel,
+    Checkbox,
+    Collapse,
+    Datepicker,
+    Datetimepicker,
+    Dropdown,
+    Field,
+    Icon,
+    Input,
+    Inputitems,
+    Loading,
+    Modal,
+    Notification,
+    Pagination,
+    Radio,
+    Select,
+    Skeleton,
+    Sidebar,
+    Slider,
+    Steps,
+    Switch,
+    Table,
+    Tabs,
+    Timepicker,
+    Tooltip,
+    Upload
+}
