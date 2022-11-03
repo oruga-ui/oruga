d881573fc5f6a76e71f08a0b41739a847a071974
Andrea Stagi <stagi.andrea@gmail.com>
2022-11-03 22:26:24 +0100

    chore: final configuration for cypress >= 10

--- packages/oruga-next/vite.config.js
+++ packages/oruga-next/vite.config.js
@@ -1,16 +1,20 @@
-import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import istanbul from 'vite-plugin-istanbul'
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default {
+    open: true,
+    port: 3000,
+    build: {
+        sourcemap: true
+    },
     plugins: [
         vue(),
         istanbul({
             include: 'src/*',
             exclude: ["node_modules", "cypress/", "**/tests/*"],
             extension: [".ts", ".vue"],
-            cypress: true
+            cypress: true,
         })
     ]
-})
+}
