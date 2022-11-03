66e9f3655e905ba63f5146e91db975721d32f430
Andrea Stagi <stagi.andrea@gmail.com>
2022-11-03 09:11:32 +0100

    chore: rename tests

---
+++ packages/oruga-next/src/components/button/tests/Button.spec.cy.ts
@@ -0,0 +1,14 @@
+import OButton from '../Button.vue'
+
+describe('<Button>', () => {
+    it('works', () => {
+        cy.mount(OButton, {
+            slots: {
+                default: {
+                    render: () => 'Hello Cypress!'
+                }
+            }
+        })
+        cy.get('button').contains('Hello Cypress!').click()
+    })
+})
