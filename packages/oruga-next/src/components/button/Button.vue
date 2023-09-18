9fe8e021e41e41833fae26cae161ffe66a6e4422
mlmo <marcel.moravek@yahoo.de>
2023-09-18 22:44:02 +0200

    refactor(button)

--- packages/oruga-next/src/components/button/Button.vue
+++ packages/oruga-next/src/components/button/Button.vue
@@ -1,11 +1,216 @@
+<script setup lang="ts">
+import { computed, type Component, type PropType } from "vue";
+
+import OIcon from "../icon/Icon.vue";
+
+import { getOptions } from "@/utils/config";
+import { getValueByPath } from "@/utils/helpers";
+import { useComputedClass } from "@/composables";
+
+/**
+ * The classic button, in different colors, sizes, and states
+ * @displayName Button
+ * @style _button.scss
+ */
+defineOptions({
+    isOruga: true,
+    name: "OButton",
+    configField: "button",
+    inheritAttrs: false,
+});
+
+// mixins: [BaseComponentMixin],
+const props = defineProps({
+    override: Boolean,
+    /**
+     * Color of the control, optional
+     * @values primary, info, success, warning, danger, and any other custom color
+     */
+    variant: String,
+    /**
+     * Size of button, optional
+     * @values small, medium, large
+     */
+    size: String,
+    /**
+     * Button label, optional when default slot
+     */
+    label: String,
+    /**
+     * Icon pack to use
+     * @values mdi, fa, fas and any other custom icon pack
+     */
+    iconPack: String,
+    /**
+     * Icon name to show on the left
+     */
+    iconLeft: String,
+    /**
+     * Icon name to show on the right
+     */
+    iconRight: String,
+    /**
+     * Rounded style
+     */
+    rounded: {
+        type: Boolean,
+        default: () => {
+            return getValueByPath(getOptions(), "button.rounded", false);
+        },
+    },
+    /**
+     * Outlined style
+     */
+    outlined: Boolean,
+    /**
+     * Loading style
+     */
+    loading: Boolean,
+    /**
+     * Button will be expanded (full-width)
+     */
+    expanded: Boolean,
+    inverted: Boolean,
+    /**
+     * Button type, like native
+     */
+    nativeType: {
+        type: String,
+        default: "button",
+        validator: (value: string) => {
+            return ["button", "submit", "reset"].indexOf(value) >= 0;
+        },
+    },
+    /**
+     * Button tag name
+     * @values button, a, input, router-link, nuxt-link (or other nuxt alias)
+     */
+    tag: {
+        type: [String, Object, Function] as PropType<string | Component>,
+        default: "button",
+    },
+    /**
+     * Button will be disabled
+     */
+    disabled: Boolean,
+    /**  @ignore */
+    iconBoth: Boolean, // This is used internally
+    elementsWrapperClass: [String, Function, Array],
+    rootClass: [String, Function, Array],
+    outlinedClass: [String, Function, Array],
+    loadingClass: [String, Function, Array],
+    invertedClass: [String, Function, Array],
+    expandedClass: [String, Function, Array],
+    roundedClass: [String, Function, Array],
+    disabledClass: [String, Function, Array],
+    iconClass: [String, Function, Array],
+    iconLeftClass: [String, Function, Array],
+    iconRightClass: [String, Function, Array],
+    labelClass: [String, Function, Array],
+    sizeClass: [String, Function, Array],
+    variantClass: [String, Function, Array],
+});
+
+const rootClasses = computed(() => {
+    return [
+        useComputedClass("rootClass", "o-btn"),
+        {
+            [useComputedClass("sizeClass", "o-btn--", props.size)]: props.size,
+        },
+        {
+            [useComputedClass("variantClass", "o-btn--", props.variant)]:
+                props.variant,
+        },
+        {
+            [useComputedClass("outlinedClass", "o-btn--outlined")]:
+                props.outlined && !props.variant,
+        },
+        {
+            [useComputedClass("invertedClass", "o-btn--inverted")]:
+                props.inverted && !props.variant,
+        },
+        {
+            [useComputedClass(
+                "outlinedClass",
+                "o-btn--outlined-",
+                props.variant,
+            )]: props.outlined && props.variant,
+        },
+        {
+            [useComputedClass(
+                "invertedClass",
+                "o-btn--inverted-",
+                props.variant,
+            )]: props.inverted && props.variant,
+        },
+        {
+            [useComputedClass("expandedClass", "o-btn--expanded")]:
+                props.expanded,
+        },
+        {
+            [useComputedClass("loadingClass", "o-btn--loading")]: props.loading,
+        },
+        {
+            [useComputedClass("roundedClass", "o-btn--rounded")]: props.rounded,
+        },
+        {
+            [useComputedClass("disabledClass", "o-btn--disabled")]:
+                props.disabled,
+        },
+    ];
+});
+
+const labelClasses = computed(() => {
+    return [useComputedClass("labelClass", "o-btn__label")];
+});
+
+const iconClasses = computed(() => {
+    return [useComputedClass("iconClass", "o-btn__icon")];
+});
+
+const iconLeftClasses = computed(() => {
+    return [
+        ...iconClasses.value,
+        useComputedClass("iconLeftClass", "o-btn__icon-left"),
+    ];
+});
+
+const iconRightClasses = computed(() => {
+    return [
+        ...iconClasses.value,
+        useComputedClass("iconRightClass", "o-btn__icon-right"),
+    ];
+});
+const elementsWrapperClasses = computed(() => {
+    return [useComputedClass("elementsWrapperClass", "o-btn__wrapper")];
+});
+const computedTag = computed(() => {
+    if (typeof props.disabled !== "undefined" && props.disabled !== false) {
+        return "button";
+    }
+    return props.tag;
+});
+
+const computedNativeType = computed(() => {
+    if (props.tag === "button" || props.tag === "input") {
+        return props.nativeType;
+    }
+    return null;
+});
+
+const computedDisabled = computed(() => {
+    if (props.disabled) return true;
+    return null;
+});
+</script>
+
 <template>
     <component
         v-bind="$attrs"
-        :disabled="computedDisabled"
         :is="computedTag"
+        :disabled="computedDisabled"
         :type="computedNativeType"
-        :class="rootClasses"
-    >
+        :class="rootClasses">
         <span :class="elementsWrapperClasses">
             <o-icon
                 v-if="iconLeft"
@@ -13,11 +218,8 @@
                 :icon="iconLeft"
                 :size="size"
                 :both="iconBoth"
-                :class="iconLeftClasses"
-            />
-            <span
-                :class="labelClasses"
-                v-if="label || $slots.default">
+                :class="iconLeftClasses" />
+            <span v-if="label || $slots.default" :class="labelClasses">
                 <slot>{{ label }}</slot>
             </span>
             <o-icon
@@ -26,185 +228,7 @@
                 :icon="iconRight"
                 :size="size"
                 :both="iconBoth"
-                :class="iconRightClasses"
-            />
+                :class="iconRightClasses" />
         </span>
     </component>
 </template>
-
-<script lang="ts">
-import type { Component, PropType } from 'vue'
-import { defineComponent } from 'vue'
-
-import Icon from '../icon/Icon.vue'
-
-import { getOptions } from '../../utils/config'
-import BaseComponentMixin from '../../utils/BaseComponentMixin'
-import { getValueByPath } from '../../utils/helpers'
-
-/**
- * The classic button, in different colors, sizes, and states
- * @displayName Button
- * @style _button.scss
- */
-export default defineComponent({
-    name: 'OButton',
-    components: {
-        [Icon.name]: Icon
-    },
-    configField: 'button',
-    mixins: [BaseComponentMixin],
-    inheritAttrs: false,
-    props: {
-        /**
-        * Color of the control, optional
-        * @values primary, info, success, warning, danger, and any other custom color
-        */
-        variant: String,
-        /**
-         * Size of button, optional
-         * @values small, medium, large
-         */
-        size: String,
-        /**
-         * Button label, optional when default slot
-         */
-        label: String,
-        /**
-         * Icon pack to use
-         * @values mdi, fa, fas and any other custom icon pack
-         */
-        iconPack: String,
-        /**
-         * Icon name to show on the left
-         */
-        iconLeft: String,
-        /**
-         * Icon name to show on the right
-         */
-        iconRight: String,
-        /**
-         * Rounded style
-         */
-        rounded: {
-            type: Boolean,
-            default: () => { return getValueByPath(getOptions(), 'button.rounded', false) }
-        },
-        /**
-         * Outlined style
-         */
-        outlined: Boolean,
-        /**
-         * Loading style
-         */
-        loading: Boolean,
-        /**
-         * Button will be expanded (full-width)
-         */
-        expanded: Boolean,
-        inverted: Boolean,
-        /**
-         * Button type, like native
-         */
-        nativeType: {
-            type: String,
-            default: 'button',
-            validator: (value: string) => {
-                return [
-                    'button',
-                    'submit',
-                    'reset'
-                ].indexOf(value) >= 0
-            }
-        },
-        /**
-         * Button tag name
-         * @values button, a, input, router-link, nuxt-link (or other nuxt alias)
-         */
-        tag: {
-            type: [String, Object, Function] as PropType<string | Component>,
-            default: 'button'
-        },
-        /**
-         * Button will be disabled
-         */
-        disabled: Boolean,
-        /**  @ignore */
-        iconBoth: Boolean, // This is used internally
-        elementsWrapperClass: [String, Function, Array],
-        rootClass: [String, Function, Array],
-        outlinedClass: [String, Function, Array],
-        loadingClass: [String, Function, Array],
-        invertedClass: [String, Function, Array],
-        expandedClass: [String, Function, Array],
-        roundedClass: [String, Function, Array],
-        disabledClass: [String, Function, Array],
-        iconClass: [String, Function, Array],
-        iconLeftClass: [String, Function, Array],
-        iconRightClass: [String, Function, Array],
-        labelClass: [String, Function, Array],
-        sizeClass: [String, Function, Array],
-        variantClass: [String, Function, Array]
-    },
-    computed: {
-        rootClasses() {
-            return [
-                this.computedClass('rootClass', 'o-btn'),
-                { [this.computedClass('sizeClass', 'o-btn--', this.size)]: this.size },
-                { [this.computedClass('variantClass', 'o-btn--', this.variant)]: this.variant },
-                { [this.computedClass('outlinedClass', 'o-btn--outlined')]: this.outlined && !this.variant },
-                { [this.computedClass('invertedClass', 'o-btn--inverted')]: this.inverted && !this.variant },
-                { [this.computedClass('outlinedClass', 'o-btn--outlined-', this.variant)]: this.outlined && this.variant },
-                { [this.computedClass('invertedClass', 'o-btn--inverted-', this.variant)]: this.inverted && this.variant },
-                { [this.computedClass('expandedClass', 'o-btn--expanded')]: this.expanded },
-                { [this.computedClass('loadingClass', 'o-btn--loading')]: this.loading },
-                { [this.computedClass('roundedClass', 'o-btn--rounded')]: this.rounded },
-                { [this.computedClass('disabledClass', 'o-btn--disabled')]: this.disabled },
-            ]
-        },
-        labelClasses() {
-            return [
-                this.computedClass('labelClass', 'o-btn__label'),
-            ]
-        },
-        iconClasses() {
-            return [
-                this.computedClass('iconClass', 'o-btn__icon'),
-            ]
-        },
-        iconLeftClasses() {
-            return [
-                ...this.iconClasses,
-                this.computedClass('iconLeftClass', 'o-btn__icon-left')
-            ]
-        },
-        iconRightClasses() {
-            return [
-                ...this.iconClasses,
-                this.computedClass('iconRightClass', 'o-btn__icon-right')
-            ]
-        },
-        elementsWrapperClasses() {
-            return [
-                this.computedClass('elementsWrapperClass', 'o-btn__wrapper'),
-            ]
-        },
-        computedTag() {
-            if (typeof this.disabled !== 'undefined' && this.disabled !== false) {
-                return 'button'
-            }
-            return this.tag
-        },
-        computedNativeType() {
-            if (this.tag === 'button' || this.tag === 'input') {
-                return this.nativeType
-            }
-            return null
-        },
-        computedDisabled() {
-            if (this.disabled) return true
-            return null
-        }
-    }
-})
-</script>
---
+++ packages/oruga-next/src/composables/useComputedClass.ts
@@ -0,0 +1,102 @@
+import { getOptions } from "@/utils/config";
+import { blankIfUndefined, endsWith, getValueByPath } from "@/utils/helpers";
+import type { ComponentOptions } from "vue";
+
+const _defaultSuffixProcessor = (input: string, suffix: string) => {
+    return blankIfUndefined(input)
+        .split(" ")
+        .filter((cls) => cls.length > 0)
+        .map((cls) => cls + suffix)
+        .join(" ");
+};
+
+const _getContext = (vm: ComponentOptions) => {
+    const computedNames = vm.$options.computed
+        ? Object.keys(vm.$options.computed)
+        : [];
+    const computed = computedNames
+        .filter((e) => !endsWith(e, "Classes"))
+        .reduce((o, key) => {
+            o[key] = vm[key];
+            return o;
+        }, {});
+    return { props: vm.$props, data: vm.$data, computed };
+};
+
+export function useComputedClass(
+    field: string,
+    defaultValue: string,
+    suffix: string = "",
+) {
+    const config = this.$props.override === true ? {} : getOptions();
+
+    const override =
+        this.$props.override ||
+        getValueByPath(config, `${this.$options.configField}.override`, false);
+    const overrideClass = getValueByPath(
+        config,
+        `${this.$options.configField}.${field}.override`,
+        override,
+    );
+
+    const globalTransformClasses = getValueByPath(
+        config,
+        `transformClasses`,
+        undefined,
+    );
+    const localTransformClasses = getValueByPath(
+        config,
+        `${this.$options.configField}.transformClasses`,
+        undefined,
+    );
+
+    let globalClass =
+        getValueByPath(
+            config,
+            `${this.$options.configField}.${field}.class`,
+            "",
+        ) ||
+        getValueByPath(config, `${this.$options.configField}.${field}`, "");
+    let currentClass = getValueByPath(this.$props, field);
+
+    if (Array.isArray(currentClass)) {
+        currentClass = currentClass.join(" ");
+    }
+
+    if (defaultValue.search("{*}") !== -1) {
+        defaultValue = defaultValue.replace(/\{\*\}/g, suffix);
+    } else {
+        defaultValue = defaultValue + suffix;
+    }
+
+    let context = null;
+    if (typeof currentClass === "function") {
+        context = _getContext(this);
+        currentClass = currentClass(suffix, context);
+    } else {
+        currentClass = _defaultSuffixProcessor(currentClass, suffix);
+    }
+    if (typeof globalClass === "function") {
+        globalClass = globalClass(suffix, context || _getContext(this));
+    } else {
+        globalClass = _defaultSuffixProcessor(globalClass, suffix);
+    }
+    let appliedClasses = (
+        `${
+            (override && !overrideClass) || (!override && !overrideClass)
+                ? defaultValue
+                : ""
+        } ` +
+        `${blankIfUndefined(globalClass)} ` +
+        `${blankIfUndefined(currentClass)}`
+    )
+        .trim()
+        .replace(/\s\s+/g, " ");
+    if (localTransformClasses) {
+        appliedClasses = localTransformClasses(appliedClasses);
+    }
+    if (globalTransformClasses) {
+        appliedClasses = globalTransformClasses(appliedClasses);
+    }
+    return appliedClasses;
+}
