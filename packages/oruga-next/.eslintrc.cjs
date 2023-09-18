90f110cb1c34c0244eccf290a982af9120834fc4
mlmo <marcel.moravek@yahoo.de>
2023-09-18 18:26:29 +0200

    feat(lint): add lint rules

--- packages/oruga-next/.eslintrc.cjs
+++ packages/oruga-next/.eslintrc.cjs
@@ -2,29 +2,33 @@
 module.exports = {
   root: true,
   extends: [
-    // "prettier",
-    // "eslint:recommended",
-    // "plugin:prettier/recommended",
+    "prettier",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
     "plugin:vue/vue3-recommended",
-    // '@vue/typescript/recommended',
-    // "@vue/eslint-config-typescript/recommended",
-    // "@vue/eslint-config-prettier",
-    // '@vue/typescript',
+    "plugin:vuejs-accessibility/recommended",
+    "@vue/typescript/recommended",
+    "@vue/eslint-config-typescript/recommended",
+    "@vue/eslint-config-prettier",
   ],
-  // plugins: ["prettier"],
+
+  plugins: ["prettier", "vuejs-accessibility"],
   rules: {
+    "@typescript-eslint/no-explicit-any": ["warn"],
+    "@typescript-eslint/ban-types": ["warn"],
+    "@typescript-eslint/explicit-function-return-type": ["warn"],
     "vue/padding-line-between-blocks": ["error", "always"],
     "vue/multi-word-component-names": ["off"],
-    // "comma-dangle": ["error", "always-multiline"],
-    // "prettier/prettier": [
-    //   "error",
-    //   {
-    //     trailingComma: "all",
-    //     endOfLine: "auto",
-    //     bracketSameLine: true,
-    //   },
-    // ],
+    "vue/block-order": ["error", { order: ["script", "template", "style"] }],
+    "vue/block-lang": ["error", { script: { lang: "ts" } }],
+    "comma-dangle": ["error", "always-multiline"],
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "all",
+        endOfLine: "auto",
+        bracketSameLine: true,
+      },
+    ],
   },
 };
-
-// TODO: rules will be enabled later
