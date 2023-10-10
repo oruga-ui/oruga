c70a5b662074408d0c116c3898067d752b443b29
mlmo <marcel.moravek@yahoo.de>
2023-10-10 17:08:00 +0200

    reafctor(loading): update loading component

--- packages/oruga-next/src/components/loading/Loading.vue
+++ packages/oruga-next/src/components/loading/Loading.vue
@@ -1,185 +1,194 @@
-<script lang="ts">
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref, computed, onMounted, type PropType } from "vue";
 
-import Icon from "../icon/Icon.vue";
+import OIcon from "../icon/Icon.vue";
 
-import BaseComponentMixin from "../../utils/BaseComponentMixin";
-
-import { getOptions } from "../../utils/config";
-import { removeElement, getValueByPath } from "../../utils/helpers";
-import { HTMLElement } from "../../utils/ssr";
+import { baseComponentProps } from "@/utils/SharedProps";
+import { getOption } from "@/utils/config";
+import {
+    useComputedClass,
+    useClassProps,
+    usePropBinding,
+    useEventListener,
+} from "@/composables";
+import { removeElement } from "@/utils/helpers";
+import { HTMLElement, isClient } from "@/utils/ssr";
+import type { ProgrammaticInstance } from "@/types";
 
 /**
  * A simple loading overlay
  * @displayName Loading
  * @style _loading.scss
  */
-export default defineComponent({
+defineOptions({
+    isOruga: true,
     name: "OLoading",
-    components: {
-        [Icon.name]: Icon,
-    },
-    mixins: [BaseComponentMixin],
     configField: "loading",
-    props: {
-        /** Whether loading is active or not, use v-model:active to make it two-way binding */
-        active: Boolean,
-        /** @ignore */
-        programmatic: Object,
-        /** @ignore */
-        promise: Promise,
-        container: [Object, Function, HTMLElement],
-        /** Loader will overlay the full page */
-        fullPage: {
-            type: Boolean,
-            default: true,
-        },
-        /* Custom animation (transition name) */
-        animation: {
-            type: String,
-            default: () => {
-                return getValueByPath(
-                    getOptions(),
-                    "loading.animation",
-                    "fade",
-                );
-            },
-        },
-        /** Can close Loading by pressing escape or clicking outside */
-        canCancel: {
-            type: Boolean,
-            default: false,
-        },
-        /** Callback function to call after user canceled (pressed escape / clicked outside) */
-        onCancel: {
-            type: Function,
-            default: () => {},
-        },
-        /** Icon name */
-        icon: {
-            type: String,
-            default: () => {
-                return getValueByPath(getOptions(), "loading.icon", "loading");
-            },
-        },
-        /** Enable spin effect on icon */
-        iconSpin: {
-            type: Boolean,
-            default: true,
-        },
-        iconSize: {
-            type: String,
-            default: "medium",
-        },
-        rootClass: [String, Function, Array],
-        overlayClass: [String, Function, Array],
-        iconClass: [String, Function, Array],
-        fullPageClass: [String, Function, Array],
-    },
-    emits: ["update:active", "close", "update:full-page"],
-    data() {
-        return {
-            isActive: this.active || false,
-            displayInFullPage: this.fullPage,
-        };
-    },
-    computed: {
-        rootClasses() {
-            return [
-                this.computedClass("rootClass", "o-load"),
-                {
-                    [this.computedClass("fullPageClass", "o-load--fullpage")]:
-                        this.displayInFullPage,
-                },
-            ];
-        },
-        overlayClasses() {
-            return [this.computedClass("overlayClass", "o-load__overlay")];
-        },
-        iconClasses() {
-            return [this.computedClass("iconClass", "o-load__icon")];
-        },
-    },
-    watch: {
-        active(value) {
-            this.isActive = value;
-        },
-        fullPage(value) {
-            this.displayInFullPage = value;
-        },
-    },
-    created() {
-        if (typeof window !== "undefined") {
-            document.addEventListener("keyup", this.keyPress);
-        }
-    },
-    mounted() {
-        if (this.programmatic) {
-            if (this.programmatic.instances) {
-                this.programmatic.instances.add(this);
-            }
-            // Insert the Loading component in body tag
-            // only if it's programmatic
-            if (!this.container) {
-                document.body.appendChild(this.$el);
-            } else {
-                this.displayInFullPage = false;
-                this.$emit("update:full-page", false);
-                this.container.appendChild(this.$el);
-            }
-            this.isActive = true;
-        }
-    },
-    beforeUnmount() {
-        if (typeof window !== "undefined") {
-            document.removeEventListener("keyup", this.keyPress);
-        }
-    },
-    methods: {
-        /**
-         * Close the Modal if canCancel.
-         */
-        cancel(method) {
-            if (!this.canCancel || !this.isActive) return;
-
-            this.close({ action: "cancel", method });
-        },
-        /**
-         * Emit events, and destroy modal if it's programmatic.
-         */
-        close(...args: any[]) {
-            this.onCancel.apply(null, args);
-            this.$emit("close");
-            this.$emit("update:active", false);
-
-            // Timeout for the animation complete before destroying
-            if (this.programmatic) {
-                if (this.programmatic.instances) {
-                    this.programmatic.instances.remove(this);
-                }
-                if (this.programmatic.resolve) {
-                    this.programmatic.resolve.apply(null, args);
-                }
-                this.isActive = false;
-                window.requestAnimationFrame(() => {
-                    removeElement(this.$el);
-                });
-            }
-        },
-        /**
-         * Keypress event that is bound to the document.
-         */
-        keyPress({ key }) {
-            if (key === "Escape" || key === "Esc") this.cancel("escape");
-        },
-    },
+    inheritAttrs: false,
 });
+
+const props = defineProps({
+    // add global shared props (will not be displayed in the docs)
+    ...baseComponentProps,
+    /** Whether loading is active or not, use v-model:active to make it two-way binding. */
+    active: { type: Boolean, default: false },
+    /** Loader will overlay the full page. */
+    fullPage: { type: Boolean, default: true },
+    /** Notification label, unnecessary when default slot is used. */
+    label: { type: String, default: undefined },
+    /** Custom animation (transition name) */
+    animation: {
+        type: String,
+        default: () => getOption("loading.animation", "fade"),
+    },
+    /** Can cancle Loading by pressing escape or clicking outside. */
+    cancelable: { type: Boolean, default: false },
+    /** Callback function to call after user canceled (pressed escape / clicked outside). */
+    onCancle: { type: Function, default: () => {} },
+    /** Icon name to show, unnecessary when default slot is used. */
+    icon: {
+        type: String,
+        default: () => getOption("loading.icon", "loading"),
+    },
+    /** Enable spin effect on icon */
+    iconSpin: {
+        type: Boolean,
+        default: () => getOption("loading.iconSpin", true),
+    },
+    /**
+     * Icon size
+     * @values small, medium, large
+     */
+    iconSize: {
+        type: String,
+        default: () => getOption("loading.iconSize", "medium"),
+    },
+    /**
+     * DOM element where the loading component will be created on.
+     * Note that this also changes fullPage to false.
+     */
+    container: { type: HTMLElement, default: undefined },
+    /**
+     * This is used internally for programmatic usage
+     * @ignore
+     */
+    programmatic: {
+        type: Object as PropType<ProgrammaticInstance<typeof this>>,
+        default: undefined,
+    },
+    // add class props (will not be displayed in the docs)
+    ...useClassProps([
+        "rootClass",
+        "overlayClass",
+        "fullPageClass",
+        "iconClass",
+        "labelClass",
+    ]),
+});
+
+const emits = defineEmits<{
+    /**
+     * active prop two-way binding
+     * @param value {boolean} updated active prop
+     */
+    (e: "update:active", value: boolean): void;
+    /**
+     * on loading close event
+     * @param value {any}
+     */
+    (e: "close", ...args: any[]): void;
+}>();
+
+const rootRef = ref();
+
+const isActive = usePropBinding("active", props, emits);
+const displayInFullPage = usePropBinding("fullPage", props, emits);
+
+if (isClient)
+    useEventListener("keyup", onKeyPress, props.container || document);
+
+onMounted(() => {
+    if (props.programmatic) {
+        if (props.programmatic.instances) {
+            props.programmatic.instances.add(this);
+        }
+        // Insert the Loading component in body tag
+        // only if it's programmatic
+        if (!props.container) {
+            document.body.appendChild(rootRef.value);
+        } else {
+            displayInFullPage.value = false;
+            props.container.appendChild(rootRef.value);
+        }
+        isActive.value = true;
+    }
+});
+
+/** Close the Modal if canCancel. */
+function cancel(method: string): void {
+    if (!props.cancelable || !isActive.value) return;
+    close({ action: "cancel", method });
+}
+
+/** Emit events, and destroy modal if it's programmatic. */
+function close(...args: any[]): void {
+    emits("close");
+    props.onCancle.apply(null, args);
+
+    // Timeout for the animation complete before destroying
+    if (props.programmatic) {
+        if (props.programmatic.instances)
+            props.programmatic.instances.remove(this);
+
+        if (props.programmatic.resolve)
+            props.programmatic.resolve.apply(null, args);
+
+        // Timeout for the animation complete before destroying
+        setTimeout(() => {
+            isActive.value = false;
+            window.requestAnimationFrame(() => removeElement(rootRef.value));
+        });
+    } else isActive.value = false;
+}
+
+/** Keypress event that is bound to the document. */
+function onKeyPress(event: KeyboardEvent): void {
+    if (event.key === "Escape" || event.key === "Esc") cancel("escape");
+}
+
+// --- Computed Component Classes ---
+
+const rootClasses = computed(() => [
+    useComputedClass("rootClass", "o-load"),
+    {
+        [useComputedClass("fullPageClass", "o-load--fullpage")]:
+            displayInFullPage.value,
+    },
+]);
+
+const overlayClasses = computed(() => [
+    useComputedClass("overlayClass", "o-load__overlay"),
+]);
+
+const iconClasses = computed(() => [
+    useComputedClass("iconClass", "o-load__icon"),
+]);
+
+const labelClasses = computed(() => [
+    useComputedClass("labelClass", "o-load__label"),
+]);
+
+// --- Expose Public Functionality ---
+
+/** expose close function for programmatic usage */
+defineExpose({ close });
 </script>
 
 <template>
     <transition :name="animation">
-        <div v-if="isActive" :class="rootClasses">
+        <div v-if="isActive" ref="rootRef" :class="rootClasses">
             <div :class="overlayClasses" @click="cancel('outside')" />
+            <!-- @slot Override icon and label -->
             <slot>
                 <o-icon
                     :icon="icon"
@@ -187,6 +196,9 @@ export default defineComponent({
                     :size="iconSize"
                     :class="iconClasses"
                     both />
+                <span v-if="label" :class="labelClasses">
+                    {{ label }}
+                </span>
             </slot>
         </div>
     </transition>
