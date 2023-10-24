1d8e31a5b508ebfc8b8e76c1953249dd15a1b3cf
mlmo <marcel.moravek@yahoo.de>
2023-10-24 11:50:37 +0200

    update(select): export options type

--- packages/oruga-next/src/components/select/index.ts
+++ packages/oruga-next/src/components/select/index.ts
@@ -4,10 +4,15 @@ import Select from "./Select.vue";
 
 import { registerComponent } from "@/utils/plugins";
 
+// export select specific types
+export type { OptionsItem } from "./types";
+
+// export select plugin
 export default {
     install(app: App) {
         registerComponent(app, Select);
     },
 } as Plugin;
 
+// export select components
 export { Select as OSelect };
