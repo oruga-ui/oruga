b67c45c2af83baa4950e909ad911189e8dad0a3c
mlmo <marcel.moravek@yahoo.de>
2023-10-24 11:48:49 +0200

    update(select): add options prop

--- packages/oruga-next/src/components/select/Select.vue
+++ packages/oruga-next/src/components/select/Select.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, watch, onMounted, ref, nextTick } from "vue";
+import { computed, watch, onMounted, ref, nextTick, type PropType } from "vue";
 
 import OIcon from "../icon/Icon.vue";
 
@@ -13,6 +13,8 @@ import {
 } from "@/composables";
 
 import { injectField } from "../field/useFieldShare";
+
+import type { OptionsItem } from "./types";
 
 /**
  * Select an item in a dropdown list. Use with Field to access all functionalities
@@ -33,6 +35,11 @@ const props = defineProps({
     modelValue: {
         type: [String, Number, Boolean, Object, Array],
         default: null,
+    },
+    /** Select options, unnecessary when default slot is used */
+    options: {
+        type: Array as PropType<OptionsItem[]>,
+        default: undefined,
     },
     /**
      * Vertical size of input, optional
@@ -293,13 +300,29 @@ const iconRightClasses = computed(() => [
             @blur="onBlur"
             @focus="onFocus"
             @invalid="onInvalid">
-            <template v-if="placeholder">
+            <template v-if="placeholder || $slots.placeholder">
                 <option v-if="placeholderVisible" :value="null" disabled hidden>
-                    {{ placeholder }}
+                    <!-- 
+                        @slot Override the placeholder
+                    -->
+                    <slot name="placeholder">
+                        {{ placeholder }}
+                    </slot>
                 </option>
             </template>
 
-            <slot />
+            <!--
+                @slot Override the options
+            -->
+            <slot>
+                <option
+                    v-for="option in options"
+                    :key="String(option.value)"
+                    :value="option.value"
+                    v-bind="option.attrs">
+                    {{ option.label }}
+                </option>
+            </slot>
         </select>
 
         <o-icon
