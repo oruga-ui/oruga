11eb3303202b5305399a4d79836b246b19388228
mlmo <marcel.moravek@yahoo.de>
2023-12-21 14:03:34 +0100

    update: volar.d.ts generation

--- .scripts/gen-component-declaration.mjs
+++ .scripts/gen-component-declaration.mjs
@@ -5,58 +5,60 @@ import process from 'process'
 
 const componentDirectory = './src/components';
 
-const TYPE_ROOT = process.cwd()
-const excludeComponents = ['']
+// Components to be ignored for creating volar type 
+const IGNORE = [
+  "FieldBody",
+  "SliderThumb",
+  "TableMobileSort",
+  "TablePagination",
+  "PaginationButton",
+  "DatepickerTable",
+  "DatepickerTableRow",
+  "DatepickerMonth",
+  "PickerWrapper",
+  "NotificationNotice",
+];
 
-const getDirectories = source =>
-  fs.readdirSync(source, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => dirent.name)
+const TYPE_ROOT = process.cwd()
+
+function getComponents(dir) {
+  const files = fs.readdirSync(dir, { recursive: true });
+  return files
+    // filter only vue files and remove test files
+    .filter(f => f.includes(".vue") && !f.includes("tests"))
+    // remove path
+    .map(f => f.includes("\\") ? f.substring(f.lastIndexOf("\\") + 1) : f)
+    // remove .vue suffix
+    .map(f => f.substring(0, f.indexOf(".vue")))
+}
 
 function exist (path) {
   return fs.existsSync(path)
-}
-
-function parseComponentsDeclaration (code) {
-  if (!code) return {}
-  return Object.fromEntries(
-    Array.from(code.matchAll(/(?<!\/\/)\s+\s+['"]?(.+?)['"]?:\s(.+?)\n/g)).map(
-      (i) => [i[1], i[2]]
-    )
-  )
 }
 
 function generateComponentsType () {  
   if(!exist(path.resolve(TYPE_ROOT, componentDirectory))) 
     throw new Error("Path not exist: " + componentDirectory);
 
-  const componentDirectories = getDirectories(componentDirectory);
-
-  const globalComponents = componentDirectories.map((dir) => "O" + dir[0].toUpperCase() + dir.substring(1));
+  const globalComponents = getComponents(componentDirectory);
 
   const components = {}
-  globalComponents.filter((key) => !excludeComponents.includes(key)).forEach((key) => {
+  globalComponents
+    .filter((key) => !IGNORE.includes(key))
+    .map((dir) => "O" + dir)
+    .forEach((key) => {
     components[key] = `(typeof import("@oruga-ui/oruga-next"))["${key}"];`;
-  })
+  });
 
-  const originalContent = exist(path.resolve(TYPE_ROOT, 'volar.d.ts'))
-    ? fs.readFileSync(path.resolve(TYPE_ROOT, 'volar.d.ts'), 'utf-8')
-    : ''
-
-  const originImports = parseComponentsDeclaration(originalContent)
-  const lines = Object.entries({
-    ...originImports,
-    ...components
-  })
-    .filter(([name]) => {
-      return components[name]
-    })
+  const lines = Object.entries(components)
+    .filter(([name]) => components[name])
     .map(([name, v]) => {
       if (!/^\w+$/.test(name)) {
         name = `'${name}'`
       }
       return `${name}: ${v}`
-    })
+    });
+
   const code = `// Auto generated component declarations
 declare module "vue" {
   export interface GlobalComponents {
@@ -64,10 +66,9 @@ declare module "vue" {
   }
 }
 export {};
-`
-  if (code !== originalContent) {
-    fs.writeFileSync(path.resolve(TYPE_ROOT, 'volar.d.ts'), code, 'utf-8')
-  }
+`;
+
+  fs.writeFileSync(path.resolve(TYPE_ROOT, 'volar.d.ts'), code, 'utf-8')
 }
 
 generateComponentsType()
