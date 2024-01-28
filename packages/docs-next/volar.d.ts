0719abb0921397f90aa7c9466dee590fb36505ba
mm <25961416+mlmoravek@users.noreply.github.com>
2024-01-28 19:25:21 +0100

    docs: add tsconfig volar types (#749)

---
+++ packages/docs-next/volar.d.ts
@@ -0,0 +1,42 @@
+// Auto generated component declarations
+declare module "vue" {
+    export interface GlobalComponents {
+        OAutocomplete: (typeof import("../oruga-next/dist/types"))["OAutocomplete"];
+        OButton: (typeof import("../oruga-next/dist/types"))["OButton"];
+        OCarousel: (typeof import("../oruga-next/dist/types"))["OCarousel"];
+        OCarouselItem: (typeof import("../oruga-next/dist/types"))["OCarouselItem"];
+        OCheckbox: (typeof import("../oruga-next/dist/types"))["OCheckbox"];
+        OCollapse: (typeof import("../oruga-next/dist/types"))["OCollapse"];
+        ODatepicker: (typeof import("../oruga-next/dist/types"))["ODatepicker"];
+        ODatetimepicker: (typeof import("../oruga-next/dist/types"))["ODatetimepicker"];
+        ODropdown: (typeof import("../oruga-next/dist/types"))["ODropdown"];
+        ODropdownItem: (typeof import("../oruga-next/dist/types"))["ODropdownItem"];
+        OField: (typeof import("../oruga-next/dist/types"))["OField"];
+        OIcon: (typeof import("../oruga-next/dist/types"))["OIcon"];
+        OInput: (typeof import("../oruga-next/dist/types"))["OInput"];
+        OLoading: (typeof import("../oruga-next/dist/types"))["OLoading"];
+        OMenu: (typeof import("../oruga-next/dist/types"))["OMenu"];
+        OMenuItem: (typeof import("../oruga-next/dist/types"))["OMenuItem"];
+        OModal: (typeof import("../oruga-next/dist/types"))["OModal"];
+        ONotification: (typeof import("../oruga-next/dist/types"))["ONotification"];
+        OPagination: (typeof import("../oruga-next/dist/types"))["OPagination"];
+        ORadio: (typeof import("../oruga-next/dist/types"))["ORadio"];
+        OSelect: (typeof import("../oruga-next/dist/types"))["OSelect"];
+        OSidebar: (typeof import("../oruga-next/dist/types"))["OSidebar"];
+        OSkeleton: (typeof import("../oruga-next/dist/types"))["OSkeleton"];
+        OSlider: (typeof import("../oruga-next/dist/types"))["OSlider"];
+        OSliderTick: (typeof import("../oruga-next/dist/types"))["OSliderTick"];
+        OStepItem: (typeof import("../oruga-next/dist/types"))["OStepItem"];
+        OSteps: (typeof import("../oruga-next/dist/types"))["OSteps"];
+        OSwitch: (typeof import("../oruga-next/dist/types"))["OSwitch"];
+        OTable: (typeof import("../oruga-next/dist/types"))["OTable"];
+        OTableColumn: (typeof import("../oruga-next/dist/types"))["OTableColumn"];
+        OTabItem: (typeof import("../oruga-next/dist/types"))["OTabItem"];
+        OTabs: (typeof import("../oruga-next/dist/types"))["OTabs"];
+        OTaginput: (typeof import("../oruga-next/dist/types"))["OTaginput"];
+        OTimepicker: (typeof import("../oruga-next/dist/types"))["OTimepicker"];
+        OTooltip: (typeof import("../oruga-next/dist/types"))["OTooltip"];
+        OUpload: (typeof import("../oruga-next/dist/types"))["OUpload"];
+    }
+}
+export {};
