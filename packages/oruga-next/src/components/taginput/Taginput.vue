47fcd38db4c551a3b29d4a91d67a1310a7023b8d
mlmo <marcel.moravek@yahoo.de>
2024-02-09 16:39:02 +0100

    fix(taginput): fix rootClass inheritance

--- packages/oruga-next/src/components/taginput/Taginput.vue
+++ packages/oruga-next/src/components/taginput/Taginput.vue
@@ -9,6 +9,7 @@ import { getOption } from "@/utils/config";
 import { getValueByPath } from "@/utils/helpers";
 import {
     defineClasses,
+    getActiveClasses,
     useVModelBinding,
     useInputHandler,
 } from "@/composables";
@@ -421,9 +422,9 @@ const autocompleteInputClasses = defineClasses([
 
 const autocompleteBind = computed(() => ({
     ...attrs,
-    "root-class": autocompleteRootClasses.value,
+    "root-class": getActiveClasses(autocompleteRootClasses.value),
     "input-classes": {
-        "input-class": autocompleteInputClasses.value,
+        "input-class": getActiveClasses(autocompleteInputClasses.value),
     },
     ...props.autocompleteClasses,
 }));
