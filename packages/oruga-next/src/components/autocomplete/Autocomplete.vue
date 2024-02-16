af4d1837f3f8b887ce1f25e604b794cca4b9efd0
Ben Webb <ben.webb@whiplash.com>
2024-02-16 15:22:54 -0500

    fixed bug where empty slot was not shown when results were empty

--- packages/oruga-next/src/components/autocomplete/Autocomplete.vue
+++ packages/oruga-next/src/components/autocomplete/Autocomplete.vue
@@ -442,7 +442,7 @@ const isEmpty = computed(
 );
 
 watch(isEmpty, (empty) => {
-    if (isFocused) isActive.value = !empty;
+    if (isFocused.value) isActive.value = !empty || !!slots.empty;
 });
 
 const closeableOptions = computed(() => {
@@ -679,6 +679,7 @@ function handleFocus(event: Event): void {
  * Close on blur.
  */
 function handleBlur(event: Event): void {
+    console.warn("???blur");
     isActive.value = false;
     onBlur(event);
 }
@@ -932,7 +933,7 @@ function itemOptionClasses(option): ClassBind[] {
             :tag="itemTag"
             :class="[...itemClasses, ...itemEmptyClasses]">
             <!--
-                @slot Define content for empty state 
+                @slot Define content for empty state
             -->
             <slot name="empty" />
         </o-dropdown-item>
