6eca2c2a00747f01f1b035750c6411e5de0ae50c
whiplashwebb <119433664+whiplashwebb@users.noreply.github.com>
2024-02-23 06:55:37 -0500

    refactor(sidebar): use useClickOutside() composable instead of custom listener (#786)

--- packages/oruga-next/src/components/sidebar/Sidebar.vue
+++ packages/oruga-next/src/components/sidebar/Sidebar.vue
@@ -14,6 +14,7 @@ import { isClient } from "@/utils/ssr";
 import {
     defineClasses,
     getActiveClasses,
+    useClickOutside,
     useMatchMedia,
     useProgrammaticComponent,
 } from "@/composables";
@@ -334,15 +335,18 @@ onBeforeUnmount(() => {
     }
 });
 
+let removeOutsideListener = null;
+
 /** add outside click event listener */
 function addHandler(): void {
-    if (isClient && !props.overlay)
-        setTimeout(() => document.addEventListener("click", clickedOutside));
+    if (isClient && !props.overlay) {
+        removeOutsideListener = useClickOutside(sidebarContent, clickedOutside);
+    }
 }
 
 /** remove outside click event listener */
 function removeHandler(): void {
-    document.removeEventListener("click", clickedOutside);
+    if (removeOutsideListener !== null) removeOutsideListener();
 }
 
 /** Close fixed sidebar if clicked outside. */
