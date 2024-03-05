e50cc1959f0ca1111ded4c9790f25697cb4708b1
mm <25961416+mlmoravek@users.noreply.github.com>
2024-03-05 13:45:28 +0100

    fix(ConfigPlugin): export ConfigPlugin (#824)

--- packages/oruga/src/index.ts
+++ packages/oruga/src/index.ts
@@ -3,13 +3,8 @@ import type { OrugaOptions } from "./types";
 
 import * as plugins from "./components/plugins";
 
-import { merge } from "./utils/helpers";
-import { setOptions, getOptions, ConfigProgrammatic } from "./utils/config";
-import {
-    setVueInstance,
-    registerPlugin,
-    registerComponentProgrammatic,
-} from "./utils/plugins";
+import { ConfigProgrammatic, ConfigPlugin } from "./utils/config";
+import { registerPlugin, registerComponentProgrammatic } from "./utils/plugins";
 
 // export all types
 export * from "./types";
@@ -25,6 +20,8 @@ export { LoadingProgrammatic } from "./components/loading";
 export { ModalProgrammatic } from "./components/modal";
 export { NotificationProgrammatic } from "./components/notification";
 export { ConfigProgrammatic } from "./utils/config";
+// export programmatic plugins
+export { ConfigPlugin } from "./utils/config";
 
 // export programmatic composable
 export { useOruga } from "./utils/programmatic";
@@ -32,15 +29,14 @@ export { useOruga } from "./utils/programmatic";
 // default export main oruga vue plugin
 const plugin: Plugin = {
     install(app: App, options: OrugaOptions = {}) {
-        // set global vue instance
-        setVueInstance(app);
-        // set options
-        const defaultOptions = getOptions();
-        setOptions(merge(defaultOptions, options, true));
+        // enable programmatic config registering
+        app.use(ConfigPlugin, options);
+
         // add components
         for (const componentKey in plugins) {
             registerPlugin(app, plugins[componentKey]);
         }
+
         // add programmatic config component
         registerComponentProgrammatic(app, "config", ConfigProgrammatic);
     },
