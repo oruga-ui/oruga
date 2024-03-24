38fb1a6d7f621f0eed685492b237a5320a572efb
mouadTaoussi <mouadtaoussi0@gmail.com>
2024-03-24 13:54:58 +0100

    prepare to Pull request

--- packages/docs/.vitepress/theme/examples/breadcrumb/inspector.vue
+++ packages/docs/.vitepress/theme/examples/breadcrumb/inspector.vue
@@ -114,7 +114,6 @@ const inspectData = [
 
 <template>
     <inspector-wrapper v-slot="props" :inspect-data="inspectData">
-        <o-button v-bind="props">Button</o-button>
         <o-breadcrumb v-bind="props">
             <o-breadcrumb-item tag="a" href="/" active="true" >Home</o-breadcrumb-item>
             <o-breadcrumb-item tag="a" href="/documentation/" disabled="true">Docs</o-breadcrumb-item>
