06d5eef2416d5fabc744ff4af29c51dc27fb82fe
mmoravek <marcel.moravek@yahoo.de>
2024-04-21 14:59:40 +0200

    test: update menu tests

--- packages/oruga/src/components/menu/tests/MenuExample.vue
+++ packages/oruga/src/components/menu/tests/MenuExample.vue
@@ -10,7 +10,6 @@ type MenuItemProps = ComponentProps<typeof OMenuItem>;
 defineProps({
     items: { type: Array as PropType<MenuItemProps[]>, required: true },
     label: { type: String, default: undefined },
-    enableDefaultItems: { type: Boolean, default: false },
 });
 </script>
 
@@ -21,21 +20,5 @@ defineProps({
         </template>
 
         <o-menu-item v-for="el in items" :key="el.label" v-bind="el" />
-
-        <template v-if="enableDefaultItems">
-            <o-menu-item icon="cog" expanded>
-                <template #label> Administrator </template>
-                <o-menu-item icon="user" label="Users" />
-                <o-menu-item icon="mobile">
-                    <template #label> Devices </template>
-                </o-menu-item>
-                <o-menu-item icon="money-bill" label="Payments" disabled />
-            </o-menu-item>
-            <o-menu-item icon="user" label="My Account">
-                <o-menu-item label="Account data" />
-                <o-menu-item label="Addresses" />
-                <div class="test-slot">TEST</div>
-            </o-menu-item>
-        </template>
     </o-menu>
 </template>
