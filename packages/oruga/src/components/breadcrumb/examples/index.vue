b9164166af9d833aa06692fc8b230b67aa32724d
mouadTaoussi <mouadtaoussi0@gmail.com>
2024-04-30 21:30:48 +0200

    add more examples

--- packages/oruga/src/components/breadcrumb/examples/index.vue
+++ packages/oruga/src/components/breadcrumb/examples/index.vue
@@ -2,26 +2,32 @@
 import Base from "./base.vue";
 import BaseCode from "./base.vue?raw";
 
-import Styles from "./styles.vue";
-import StylesCode from "./styles.vue?raw";
+import Separators from "./separators.vue";
+import SeparatorsCode from "./separators.vue?raw";
 
 import Icons from "./icons.vue";
 import IconsCode from "./icons.vue?raw";
 
-import Tags from "./tags.vue";
-import TagsCode from "./tags.vue?raw";
+import Sizes from "./sizes.vue";
+import SizesCode from "./sizes.vue?raw";
+
+import Positions from "./positions.vue";
+import PositionsCode from "./positions.vue?raw";
 </script>
 
 <template>
     <h3 id="base">Base</h3>
     <ExampleViewer :component="Base" :code="BaseCode" />
 
-    <h3 id="styles">States and Styles</h3>
-    <ExampleViewer :component="Styles" :code="StylesCode" />
+    <h3 id="styles">Separators</h3>
+    <ExampleViewer :component="Separators" :code="SeparatorsCode" />
 
     <h3 id="icons">Icons</h3>
     <ExampleViewer :component="Icons" :code="IconsCode" />
 
-    <h3 id="tags">Tags</h3>
-    <ExampleViewer :component="Tags" :code="TagsCode" />
+    <h3 id="sizes">Sizes</h3>
+    <ExampleViewer :component="Sizes" :code="SizesCode" />
+
+    <h3 id="positions">Sizes</h3>
+    <ExampleViewer :component="Positions" :code="PositionsCode" />
 </template>
---
+++ packages/oruga/src/components/breadcrumb/examples/sizes.vue
@@ -0,0 +1,35 @@
+<template>
+    <section class="odocs-spaced">
+        <!--  align="center" -->
+        <o-breadcrumb
+            tag="div"
+            separator="has-arrow-separator"
+            size="medium"
+            align="center">
+            <o-breadcrumb-item tag="a" href="/" active="true"
+                >Home
+            </o-breadcrumb-item>
+            <o-breadcrumb-item tag="a" href="/documentation/" disabled="true"
+                >Docs
+            </o-breadcrumb-item>
+            <o-breadcrumb-item tag="a" href="/components/Breadcrumb.html"
+                >Breadcrumb
+            </o-breadcrumb-item>
+        </o-breadcrumb>
+        <o-breadcrumb
+            tag="div"
+            separator="has-arrow-separator"
+            size="medium"
+            align="center">
+            <o-breadcrumb-item tag="router-link" to="/" active="true"
+                >Home
+            </o-breadcrumb-item>
+            <o-breadcrumb-item tag="router-link" to="/documentation/" disabled="true"
+                >Docs
+            </o-breadcrumb-item>
+            <o-breadcrumb-item tag="router-link" to="/components/Breadcrumb.html"
+                >Breadcrumb
+            </o-breadcrumb-item>
+        </o-breadcrumb>
+    </section>
+</template>
