92b30f16d7f2839f7f69c2ebf6bd9c7f0e11bedf
mm <25961416+mlmoravek@users.noreply.github.com>
2024-06-13 17:11:16 +0200

    fix(datetimepicker): fix datetimepicker initialisation bug (#948)

--- packages/oruga/src/components/datetimepicker/Datetimepicker.vue
+++ packages/oruga/src/components/datetimepicker/Datetimepicker.vue
@@ -222,14 +222,22 @@ const datepickerRef = ref<InstanceType<typeof ODatepicker>>();
 const timepickerRef = ref<InstanceType<typeof OTimepicker>>();
 const nativeInputRef = ref<InstanceType<typeof OInput>>();
 
-const timepickerProps = ref(props.timepicker);
-watch(timepickerProps.value, (value) => (timepickerProps.value = value), {
-    deep: true,
-});
-const datepickerProps = ref(props.datepicker);
-watch(datepickerProps.value, (value) => (datepickerProps.value = value), {
-    deep: true,
-});
+const timepickerProps = ref<TimepickerProps>(props.timepicker);
+watch(
+    () => props.timepicker,
+    (value) => (timepickerProps.value = value),
+    { deep: true },
+);
+const datepickerProps = ref<DatepickerProps>(props.datepicker);
+watch(
+    () => props.datepicker,
+    (value) => (datepickerProps.value = value),
+    { deep: true },
+);
+
+const isMobileNative = computed(
+    () => props.mobileNative && isMobileAgent.any(),
+);
 
 const elementRef = computed(() =>
     isMobileNative.value ? nativeInputRef.value : datepickerRef.value,
@@ -240,10 +248,6 @@ const { setFocus, onBlur, onFocus, onInvalid } = useInputHandler(
     elementRef,
     emits,
     props,
-);
-
-const isMobileNative = computed(
-    () => props.mobileNative && isMobileAgent.any(),
 );
 
 watch([() => isMobileNative.value, () => props.inline], () => {
