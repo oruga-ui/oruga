6006a84e93594f891198392f777db1ec48978fd8
mmoravek <marcel.moravek@yahoo.de>
2024-06-17 17:02:05 +0200

    test(checkbox): add checkbox tests

--- packages/oruga/src/components/checkbox/tests/checkbox.test.ts
+++ packages/oruga/src/components/checkbox/tests/checkbox.test.ts
@@ -11,6 +11,41 @@ describe("OCheckbox tests", () => {
         expect(!!wrapper.vm).toBeTruthy();
         expect(wrapper.exists()).toBeTruthy();
         expect(wrapper.attributes("data-oruga")).toBe("checkbox");
+        expect(
+            wrapper.find("label input[type=checkbox]").exists(),
+        ).toBeTruthy(); // has an input checkbox
         expect(wrapper.html()).toMatchSnapshot();
+    });
+
+    test.todo("render accordingly when has native attributes", () => {});
+
+    test.todo("render accordingly when has size prop", () => {});
+
+    test("render accordingly when has variant prop", () => {});
+
+    test("render accordingly when is disabled", () => {});
+
+    test("react accordingly when value change ", async () => {});
+
+    test("react accordingly with a object values", async () => {});
+
+    test.todo("emit input event with value when value change", async () => {
+        wrapper.setProps({ value: true });
+        await wrapper.vm.$nextTick();
+        expect(wrapper.vm.computedValue).toBeTruthy();
+
+        wrapper.vm.computedValue = false;
+        await wrapper.vm.$nextTick();
+        const valueEmitted = wrapper.emitted()["input"][0];
+        expect(valueEmitted).toContainEqual(false);
+    });
+
+    test.todo("method focus() gives focus to the input element", (done) => {
+        wrapper.vm.$refs.input.focus = jest.fn();
+        wrapper.vm.focus();
+        wrapper.vm.$nextTick(() => {
+            expect(wrapper.vm.$refs.input.focus).toHaveBeenCalled();
+            done();
+        });
     });
 });
