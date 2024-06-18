2aaf14ad2ba4c981dd8f5ace6dd708336473206d
mmoravek <marcel.moravek@yahoo.de>
2024-06-18 15:26:17 +0200

    test(checkbox): add checkbox tests

--- packages/oruga/src/components/checkbox/tests/checkbox.test.ts
+++ packages/oruga/src/components/checkbox/tests/checkbox.test.ts
@@ -1,5 +1,6 @@
-import { describe, test, expect, afterEach } from "vitest";
+import { describe, test, expect, afterEach, vi } from "vitest";
 import { enableAutoUnmount, mount } from "@vue/test-utils";
+import { nextTick } from "vue";
 
 import OCheckbox from "@/components/checkbox/Checkbox.vue";
 
@@ -17,35 +18,124 @@ describe("OCheckbox tests", () => {
         expect(wrapper.html()).toMatchSnapshot();
     });
 
-    test.todo("render accordingly when has native attributes", () => {});
+    test("render accordingly when has native attributes", () => {
+        const wrapper = mount(OCheckbox, {
+            props: {
+                autocomplete: "on",
+                nativeValue: true,
+                required: true,
+            },
+        });
 
-    test.todo("render accordingly when has size prop", () => {});
-
-    test("render accordingly when has variant prop", () => {});
-
-    test("render accordingly when is disabled", () => {});
-
-    test("react accordingly when value change ", async () => {});
-
-    test("react accordingly with a object values", async () => {});
-
-    test.todo("emit input event with value when value change", async () => {
-        wrapper.setProps({ value: true });
-        await wrapper.vm.$nextTick();
-        expect(wrapper.vm.computedValue).toBeTruthy();
-
-        wrapper.vm.computedValue = false;
-        await wrapper.vm.$nextTick();
-        const valueEmitted = wrapper.emitted()["input"][0];
-        expect(valueEmitted).toContainEqual(false);
+        const input = wrapper.find("input");
+        expect(input.exists()).toBeTruthy();
+        expect(input.attributes("value")).toBe("true");
+        expect(input.attributes("autocomplete")).toBe("on");
+        expect(input.attributes("required")).not.toBeUndefined();
     });
 
-    test.todo("method focus() gives focus to the input element", (done) => {
-        wrapper.vm.$refs.input.focus = jest.fn();
+    test("render accordingly when has size prop", () => {
+        const wrapper = mount(OCheckbox, {
+            props: { size: "large" },
+        });
+
+        expect(wrapper.classes("o-chk--large")).toBeTruthy();
+    });
+
+    test("render accordingly when has variant prop", () => {
+        const wrapper = mount(OCheckbox, {
+            props: { variant: "danger" },
+        });
+
+        expect(wrapper.classes("o-chk--danger")).toBeTruthy();
+    });
+
+    test("render accordingly when is disabled", () => {
+        const wrapper = mount(OCheckbox, {
+            props: { disabled: true },
+        });
+
+        expect(wrapper.classes("o-chk--disabled")).toBeTruthy();
+        const input = wrapper.find("input");
+        expect(input.exists()).toBeTruthy();
+        expect(input.attributes("disabled")).not.toBeUndefined();
+    });
+
+    test("react accordingly when value change ", async () => {
+        const wrapper = mount(OCheckbox);
+
+        const input = wrapper.find("input");
+        expect(input.exists()).toBeTruthy();
+
+        await input.setValue(true);
+        let emits = wrapper.emitted("update:modelValue");
+        expect(emits).toHaveLength(1);
+        expect(emits[0]).toContainEqual(true);
+        await nextTick();
+        expect(wrapper.vm.value).toEqual(true);
+
+        await input.setValue(false);
+        emits = wrapper.emitted("update:modelValue");
+        expect(emits).toHaveLength(2);
+        expect(emits[1]).toContainEqual(false);
+        expect(wrapper.vm.value).toEqual(false);
+    });
+
+    test("react accordingly when custom string values are given", async () => {
+        const trueValue = "Yes";
+        const falseValue = "No";
+        const wrapper = mount<typeof OCheckbox<string>>(OCheckbox, {
+            props: { trueValue, falseValue },
+        });
+
+        const input = wrapper.find("input");
+        expect(input.exists()).toBeTruthy();
+
+        await input.setValue(true);
+        let emits = wrapper.emitted("update:modelValue");
+        expect(emits).toHaveLength(1);
+        expect(emits[0]).toContainEqual(trueValue);
+        expect(wrapper.vm.value).toEqual(trueValue);
+
+        await input.setValue(false);
+        emits = wrapper.emitted("update:modelValue");
+        expect(emits).toHaveLength(2);
+        expect(emits[1]).toContainEqual(falseValue);
+        expect(wrapper.vm.value).toEqual(falseValue);
+    });
+
+    test("react accordingly when custom object values are given", async () => {
+        const trueValue = { a: "a", b: "b" };
+        const falseValue = { y: "y", x: "X" };
+        const wrapper = mount<typeof OCheckbox<object>>(OCheckbox, {
+            props: { modelValue: falseValue, trueValue, falseValue },
+        });
+
+        const input = wrapper.find("input");
+        expect(input.exists()).toBeTruthy();
+
+        await input.setValue(true);
+        let emits = wrapper.emitted("update:modelValue");
+        expect(emits).toHaveLength(1);
+        expect(emits[0]).toContainEqual(trueValue);
+        expect(wrapper.vm.value).toEqual(trueValue);
+
+        await input.setValue(false);
+        emits = wrapper.emitted("update:modelValue");
+        expect(emits).toHaveLength(2);
+        expect(emits[1]).toContainEqual(falseValue);
+        expect(wrapper.vm.value).toEqual(falseValue);
+    });
+
+    test("react accordingly when method focus() is called", async () => {
+        const wrapper = mount(OCheckbox);
+
+        const input = wrapper.find("input");
+        input.element.focus = vi.fn();
+
         wrapper.vm.focus();
-        wrapper.vm.$nextTick(() => {
-            expect(wrapper.vm.$refs.input.focus).toHaveBeenCalled();
-            done();
+        await nextTick(() => {
+            expect(input.element.focus).toHaveBeenCalled();
         });
     });
 });
