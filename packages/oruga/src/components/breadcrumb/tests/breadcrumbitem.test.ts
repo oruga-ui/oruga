b99948d819d3622f2febbe097e7a13759d995762
mouadTaoussi <mouadtaoussi0@gmail.com>
2024-06-26 00:14:04 +0200

    prepare tests

--- packages/oruga/src/components/breadcrumb/tests/breadcrumbitem.test.ts
+++ packages/oruga/src/components/breadcrumb/tests/breadcrumbitem.test.ts
@@ -1,31 +1,34 @@
 import { describe, test, expect, vi, afterEach } from "vitest";
-import { enableAutoUnmount, mount } from "@vue/test-utils";
+import { mount } from "@vue/test-utils";
 
 import { OBreadcrumb } from "@/components/breadcrumb/Breadcrumb.vue";
 // import { OBreadcrumbItem } from "../Breadcrumbitem.vue";
 
 describe("<BreadcrumbItem>", () => {
     it("is active", () => {
-        // const triggerHTML =
-        //     '<breadcrumb-item tag="a" active="true" active-variant"primary" href="/">Home</breadcrumb-item>';
+        const triggerHTML =
+            '<breadcrumb-item tag="a" active="true" active-variant"primary" href="/">Home</breadcrumb-item>';
 
-        // const wrapper = mount(OBreadcrumb, {
-        //     slot: triggerHTML,
-        // });
+        const wrapper = mount(OBreadcrumb, {
+            slot: triggerHTML,
+        });
 
-        // expect(wrapper.classes("o-breadcrumb-item__primary")).toBeTruthy()
-        // expect(wrapper.classes("o-breadcrumb-item__primary")).toBeTruthy()
+        const trigger = wrapper.find(".o-breadcrumb-item");
+
+        expect(trigger.classes("o-breadcrumb-item__primary")).toBeTruthy();
     });
     it("is disabled", () => {
-        // const triggerHTML =
-        //     '<breadcrumb-item tag="a" disabled="true" href="/">Home</breadcrumb-item>';
+        const triggerHTML =
+            '<breadcrumb-item tag="a" disabled="true" href="/">Home</breadcrumb-item>';
 
-        // const wrapper = mount(OBreadcrumb, {
-        //     slot: triggerHTML,
-        //     props: { size: "large",},
-        // });
+        const wrapper = mount(OBreadcrumb, {
+            slot: triggerHTML,
+            props: { size: "large" },
+        });
 
-        // expect(wrapper.classes("o-breadcrumb-item__primary")).toBeTruthy()
+        const trigger = wrapper.find(".o-breadcrumb-item");
+
+        expect(trigger.classes("o-breadcrumb-item__disabled")).toBeTruthy();
     });
     it("has icon", () => {});
     it("has href link", () => {});
