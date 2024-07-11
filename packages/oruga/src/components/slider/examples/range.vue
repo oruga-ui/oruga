eafc3f3466a690f28b524f2781b8eb92760ffa76
mm <25961416+mlmoravek@users.noreply.github.com>
2024-07-11 09:42:42 +0200

    feat(slider): make `modelValue` conditional typed by prop `range` (#1018)

--- packages/oruga/src/components/slider/examples/range.vue
+++ packages/oruga/src/components/slider/examples/range.vue
@@ -8,13 +8,20 @@ const numbers2 = ref<[number, number]>([2, 6]);
 <template>
     <section>
         <o-field>
-            <o-slider v-model="numbers" :min="1" :max="15" :step="0.5" ticks />
+            <o-slider
+                v-model="numbers"
+                range
+                :min="1"
+                :max="15"
+                :step="0.5"
+                ticks />
         </o-field>
         <p>{{ numbers }}</p>
 
         <o-field>
             <o-slider
                 v-model="numbers2"
+                range
                 variant="danger"
                 :min="-2"
                 :max="8"
---
+++ packages/oruga/src/components/slider/props.ts
@@ -0,0 +1,90 @@
+import type { ComponentClass } from "@/types";
+
+export type SelectProps<IsRange extends boolean> = {
+    /** Override existing theme classes completely */
+    override?: boolean;
+    /** The input value state */
+    modelValue?: IsRange extends true ? [number, number] : number;
+    /** Enable range slider */
+    range?: IsRange;
+    /** Minimum value */
+    min?: number;
+    /** Maximum  value */
+    max?: number;
+    /** Step interval of ticks */
+    step?: number;
+    /**
+     * Color of the slider
+     * @values primary, info, success, warning, danger, and any other custom color
+     */
+    variant?: string;
+    /**
+     * Vertical size of slider
+     * @values small, medium, large
+     */
+    size?: string;
+    /** Show tick marks */
+    ticks?: boolean;
+    /** Show tooltip when thumb is being dragged */
+    tooltip?: boolean;
+    /**
+     * Color of the tooltip
+     * @values primary, info, success, warning, danger, and any other custom color
+     */
+    tooltipVariant?: string;
+    /** Tooltip displays always */
+    tooltipAlways?: boolean;
+    /** Rounded thumb */
+    rounded?: boolean;
+    /** Slider will be disabled */
+    disabled?: boolean;
+    /** Update v-model only when dragging is finished */
+    lazy?: boolean;
+    /** Function to format the tooltip label for display */
+    customFormatter?: Function;
+    /** Increases slider size on focus */
+    biggerSliderFocus?: boolean;
+    /** Show indicators */
+    indicator?: boolean;
+    /**
+     * Define v-model format
+     * @values row, percent
+     */
+    format?: "raw" | "percent";
+    /** Date format locale */
+    locale?: string | string[];
+    /** Accessibility aria-label to to be passed to the slider thumb element. */
+    ariaLabel?: string;
+} & SelectClasses;
+
+// class props (will not be displayed in the docs)
+type SelectClasses = Partial<{
+    /** Class of the root element */
+    rootClass: ComponentClass;
+    /** Class of the vertical slider size */
+    sizeClass: ComponentClass;
+    /** Class of the slider track */
+    trackClass: ComponentClass;
+    /** Class of the filled part of the slider */
+    fillClass: ComponentClass;
+    /** Class when the slider is rounded */
+    thumbRoundedClass: ComponentClass;
+    /** Class when the thumb gets dragged */
+    thumbDraggingClass: ComponentClass;
+    /** Class when slider is disabled */
+    disabledClass: ComponentClass;
+    /** Class of the thumb wrapper */
+    thumbWrapperClass: ComponentClass;
+    /** Class of the thumb */
+    thumbClass: ComponentClass;
+    /** Class of the slider variant */
+    variantClass: ComponentClass;
+    /** Class of slider tick */
+    tickClass: ComponentClass;
+    /** Class when slider tick is hidden */
+    tickHiddenClass: ComponentClass;
+    /** Class of tick label */
+    tickLabelClass: ComponentClass;
+    /** Class to display on the wrapper element when the slider is dragged */
+    thumbWrapperDraggingClass: ComponentClass;
+}>;
