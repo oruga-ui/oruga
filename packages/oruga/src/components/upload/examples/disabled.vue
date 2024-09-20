06985b4ba866faa7b19f7be30d3b9a06f84f97fa
mm <25961416+mlmoravek@users.noreply.github.com>
2024-09-20 16:53:13 +0200

    docs: enhance Upload examples (#1060)

---
+++ packages/oruga/src/components/upload/examples/disabled.vue
@@ -0,0 +1,23 @@
+<template>
+    <section>
+        <o-field>
+            <o-upload disabled>
+                <o-button tag="button" variant="primary">
+                    <o-icon icon="upload" />
+                    <span>Click to upload</span>
+                </o-button>
+            </o-upload>
+        </o-field>
+
+        <o-field>
+            <o-upload drag-drop disabled>
+                <center>
+                    <p>
+                        <o-icon icon="upload" size="is-large" />
+                    </p>
+                    <p>Drop your files here or click to upload</p>
+                </center>
+            </o-upload>
+        </o-field>
+    </section>
+</template>
--- packages/oruga/src/components/upload/examples/index.vue
+++ packages/oruga/src/components/upload/examples/index.vue
@@ -4,6 +4,12 @@ import BaseCode from "./base.vue?raw";
 
 import DragDrop from "./drag-drop.vue";
 import DragDropCode from "./drag-drop.vue?raw";
+
+import Disabled from "./disabled.vue";
+import DisabledCode from "./disabled.vue?raw";
+
+import Variants from "./variants.vue";
+import VariantsCode from "./variants.vue?raw";
 </script>
 
 <template>
@@ -12,4 +18,10 @@ import DragDropCode from "./drag-drop.vue?raw";
 
     <h3 id="drag">Drag&Drop</h3>
     <ExampleViewer :component="DragDrop" :code="DragDropCode" />
+
+    <h3 id="disabled">Disabled</h3>
+    <ExampleViewer :component="Disabled" :code="DisabledCode" />
+
+    <h3 id="variants">Variants</h3>
+    <ExampleViewer :component="Variants" :code="VariantsCode" />
 </template>
