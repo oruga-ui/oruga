6a94d1e3051de07d262ec2dc09b3ef2e0549fac1
mlmoravek <marcel.moravek@yahoo.de>
2024-09-24 11:31:14 +0200

    test: add tabs integration tests

---
+++ packages/oruga/src/components/tabs/tests/tabs.axe.test.ts
@@ -0,0 +1,57 @@
+import { afterEach, describe, expect, test } from "vitest";
+import { enableAutoUnmount, mount } from "@vue/test-utils";
+import { axe } from "jest-axe";
+
+import TabsExample from "./TabsExample.vue";
+
+describe("Tabs axe tests", () => {
+    enableAutoUnmount(afterEach);
+
+    const a11yCases = [
+        {
+            title: "axe tabs - base case",
+            props: undefined,
+        },
+        {
+            title: "axe tabs - vertical",
+            props: { vertical: true },
+        },
+        {
+            title: "axe tabs - animated",
+            props: { animated: true },
+        },
+        {
+            title: "axe tabs - multiline",
+            props: { multiline: true },
+        },
+        {
+            title: "axe tabs - expanded",
+            props: { expanded: true },
+        },
+        {
+            title: "axe tabs - boxed",
+            props: { type: "boxed" },
+        },
+        {
+            title: "axe tabs - variant",
+            props: { variant: "danger" },
+        },
+        {
+            title: "axe tabs - position",
+            props: { position: "right" },
+        },
+        {
+            title: "axe tabs - size",
+            props: { size: "large" },
+        },
+        {
+            title: "axe tabs - isStepsClickable",
+            props: { isStepsClickable: true },
+        },
+    ];
+
+    test.each(a11yCases)("$title", async ({ props }) => {
+        const wrapper = mount(TabsExample, props);
+        expect(await axe(wrapper.html())).toHaveNoViolations();
+    });
+});
