765940559f343acd90e554b9a1b1ab03660ddaca
mlmoravek <marcel.moravek@yahoo.de>
2024-11-14 18:02:18 +0100

    refactor

--- packages/oruga/src/types/utils.ts
+++ packages/oruga/src/types/utils.ts
@@ -18,11 +18,11 @@ export type PropBind =
 /** Vue native dynamic component 'is' property value type */
 export type DynamicComponent = string | object | CallableFunction | Component;
 
-/** Define a property as  required */
+/** Define a property as required */
 export type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
 
 /**
- * Extract all properties of an object which starts with an `on` prefix.
+ * Extract all properties of an object which starts with a `on` prefix.
  * It also cuts the `on` prefix and lowercase the first char.
  */
 type ComponentEmitsByProps<T> = {
@@ -34,7 +34,7 @@ type ComponentEmitsByProps<T> = {
 };
 
 /**
- * Custom ComponentEmits helper which defines the emits of an component as object.
+ * Custom type helper which extracts the `$emits` type of an component as object.
  *
  * Because the `ComponentEmit` from "vue-component-type-helpers" does not export the emits as an object but as:
  * `((event: "close", ...args: any[]) => void) | undefined`
@@ -46,7 +46,7 @@ type TypeOfKey<T, K extends string> = K extends keyof T ? T[K] : unknown;
 
 /**
  * Extracts the type of a deep property `K` within an object `T`.
- * `K` can be defined as property path with `.` seperator.
+ * Property `K` can be defined as a property path with `.` seperator.
  */
 export type DeepType<T, K> = T extends object
     ? K extends string
