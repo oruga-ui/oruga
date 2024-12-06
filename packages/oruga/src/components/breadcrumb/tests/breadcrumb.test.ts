11191e5098db8a201443fbba44571e6d762e0623
Mouad Taoussi <50294614+mouadTaoussi@users.noreply.github.com>
2024-12-06 21:46:20 +0100

    merge

--- packages/oruga/src/components/breadcrumb/tests/breadcrumb.test.ts
+++ packages/oruga/src/components/breadcrumb/tests/breadcrumb.test.ts
@@ -1,23 +1,37 @@
-import { describe, test, expect, vi, afterEach } from "vitest";
+import { describe, test, expect, afterEach } from "vitest";
 import { enableAutoUnmount, mount } from "@vue/test-utils";
-import { nextTick } from "vue";
-import { setTimeout } from "timers/promises";
+// import { nextTick } from "vue";
+// import { setTimeout } from "timers/promises";
 
-import { OBreadcrumb } from "@/components/breadcrumb/Breadcrumb.vue";
-// import { OBreadcrumbItem } from "../Breadcrumbitem.vue";
+// import { OBreadcrumb } from "@/components/breadcrumb/Breadcrumb.vue";
+import OBreadcrumb from "@/components/breadcrumb/Breadcrumb.vue";
 
 describe("<Breadcrumb>", () => {
     enableAutoUnmount(afterEach);
 
     test("is called", () => {
-        const wrapper = mount(OBreadcrumb);
+        // const wrapper = mount(OBreadcrumb);
+        const triggerHTML =
+            '<breadcrumb-item tag="a" href="/">Home</breadcrumb-item>';
+        const wrapper = mount(OBreadcrumb, {
+            slots: {
+                trigger: triggerHTML,
+            },
+        });
         expect(!!wrapper.vm).toBeTruthy();
         expect(wrapper.exists()).toBeTruthy();
         expect(wrapper.attributes("data-oruga")).toBe("breadcrumb");
     });
 
     test("rendeer correctly", () => {
-        const wrapper = mount(OBreadcrumb);
+        // const wrapper = mount(OBreadcrumb);
+        const triggerHTML =
+            '<breadcrumb-item tag="a" href="/">Home</breadcrumb-item>';
+        const wrapper = mount(OBreadcrumb, {
+            slots: {
+                trigger: triggerHTML,
+            },
+        });
         expect(wrapper.html()).toMatchSnapshot();
         expect(wrapper.classes("o-breadcrumb")).toBeTruthy();
     });
@@ -26,35 +40,39 @@ describe("<Breadcrumb>", () => {
         const triggerHTML =
             '<breadcrumb-item tag="a" href="/">Home</breadcrumb-item>';
 
+        // const wrapper = mount(OBreadcrumb, {
+        //     slot: triggerHTML,
+        // });
         const wrapper = mount(OBreadcrumb, {
-            slot: triggerHTML
+            slots: {
+                trigger: triggerHTML,
+            },
         });
-        const trigger = wrapper.find(".o-breadcrumb-item")
-        // expect(trigger.html()).toBe(triggerHTML)
-        expect(trigger.exists()).toBeTruthy()
-        expect(trigger.text()).contain("Home")
+        const trigger = wrapper.find(".o-breadcrumb-item");
+        expect(trigger.html()).toBe(triggerHTML);
+        expect(trigger.exists()).toBeTruthy();
+        // expect(trigger.text()).contain("Home");
     });
     test("is centered", () => {
         const triggerHTML =
-        '<breadcrumb-item tag="a" href="/">Home</breadcrumb-item>';
+            '<breadcrumb-item tag="a" href="/">Home</breadcrumb-item>';
 
         const wrapper = mount(OBreadcrumb, {
             slot: triggerHTML,
-            props: { align : "centered"}
+            props: { align: "centered" },
         });
 
-        expect(wrapper.classes("o-breadcrumb__centered")).toBeTruthy()
+        expect(wrapper.classes("o-breadcrumb__centered")).toBeTruthy();
     });
     test("is large ", () => {
         const triggerHTML =
-        '<breadcrumb-item tag="a" href="/">Home</breadcrumb-item>';
+            '<breadcrumb-item tag="a" href="/">Home</breadcrumb-item>';
 
         const wrapper = mount(OBreadcrumb, {
             slot: triggerHTML,
-            props: { size : "large"}
+            props: { size: "large" },
         });
 
-        expect(wrapper.classes("o-breadcrumb__large")).toBeTruthy()
+        expect(wrapper.classes("o-breadcrumb__large")).toBeTruthy();
     });
- 
 });
--- packages/oruga/src/components/breadcrumb/tests/breadcrumbitem.test.ts
+++ packages/oruga/src/components/breadcrumb/tests/breadcrumbitem.test.ts
@@ -1,33 +1,40 @@
-import { describe, test, expect, vi, afterEach } from "vitest";
+import { describe, test, expect } from "vitest";
 import { mount } from "@vue/test-utils";
 
-import { OBreadcrumb } from "@/components/breadcrumb/Breadcrumb.vue";
+import OBreadcrumb from "@/components/breadcrumb/Breadcrumb.vue";
 // import { OBreadcrumbItem } from "../Breadcrumbitem.vue";
 
 describe("<BreadcrumbItem>", () => {
     test("is active", () => {
         const triggerHTML =
             '<breadcrumb-item tag="a" active="true" active-variant"primary" href="/">Home</breadcrumb-item>';
-
+        // const wrapper = mount(OBreadcrumb, {
+        //     slot: triggerHTML,
+        // });
         const wrapper = mount(OBreadcrumb, {
-            slot: triggerHTML,
+            slots: {
+                trigger: triggerHTML,
+            },
         });
 
         const trigger = wrapper.find(".o-breadcrumb-item");
-
         expect(trigger.classes("o-breadcrumb-item__primary")).toBeTruthy();
     });
     test("is disabled", () => {
         const triggerHTML =
             '<breadcrumb-item tag="a" disabled="true" href="/">Home</breadcrumb-item>';
-
+        // const wrapper = mount(OBreadcrumb, {
+        //     slot: triggerHTML,
+        //     props: { size: "large" },
+        // });
         const wrapper = mount(OBreadcrumb, {
-            slot: triggerHTML,
+            slots: {
+                trigger: triggerHTML,
+            },
             props: { size: "large" },
         });
 
         const trigger = wrapper.find(".o-breadcrumb-item");
-
         expect(trigger.classes("o-breadcrumb-item__disabled")).toBeTruthy();
     });
     test("has icon", () => {});
