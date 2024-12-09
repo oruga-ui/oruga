4d30ed36fc67cc85a0e37c5764964685027ed559
mlmoravek <marcel.moravek@yahoo.de>
2024-12-09 10:16:21 +0100

    test: fix tests

--- packages/oruga/src/components/tabs/tests/tabs.test.ts
+++ packages/oruga/src/components/tabs/tests/tabs.test.ts
@@ -1,13 +1,16 @@
 import { describe, test, expect, afterEach } from "vitest";
 import { enableAutoUnmount, mount } from "@vue/test-utils";
+import { nextTick } from "vue";
 
 import OTabs from "@/components/tabs/Tabs.vue";
 
 describe("OTabs tests", () => {
     enableAutoUnmount(afterEach);
 
-    test("render correctly", () => {
+    test("render correctly", async () => {
         const wrapper = mount(OTabs);
+        await nextTick();
+
         expect(!!wrapper.vm).toBeTruthy();
         expect(wrapper.exists()).toBeTruthy();
         expect(wrapper.attributes("data-oruga")).toBe("tabs");
