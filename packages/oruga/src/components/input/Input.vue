dcd30a61fe635b3d8ca3510828a8aada505c7ef6
mlmoravek <marcel.moravek@yahoo.de>
2024-12-17 09:07:17 +0100

    fix(input): solve prop `number` declaration issue

--- packages/oruga/src/components/input/Input.vue
+++ packages/oruga/src/components/input/Input.vue
@@ -14,6 +14,7 @@ import {
 import OIcon from "../icon/Icon.vue";
 
 import { getDefault } from "@/utils/config";
+import { isTrueish } from "@/utils/helpers";
 import { defineClasses, useDebounce, useInputHandler } from "@/composables";
 
 import { injectField } from "../field/fieldInjection";
@@ -129,7 +130,7 @@ const vmodel = defineModel<ModelValue, string, string, ModelValue>({
     set: (value) =>
         typeof value == "undefined"
             ? value
-            : props.number
+            : isTrueish(props.number)
               ? Number(value)
               : String(value),
     default: undefined,
@@ -231,7 +232,7 @@ function iconClick(event: Event): void {
 function rightIconClick(event: Event): void {
     if (props.passwordReveal) togglePasswordVisibility();
     else if (props.clearable)
-        vmodel.value = (props.number ? 0 : "") as ModelValue;
+        vmodel.value = (isTrueish(props.number) ? 0 : "") as ModelValue;
     if (props.iconRightClickable) {
         emits("icon-right-click", event);
         nextTick(() => setFocus());
