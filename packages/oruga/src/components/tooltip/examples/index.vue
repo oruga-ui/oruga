d43a21c4d1eb30aa44ab7d483259f2d2edaeca4a
mlmoravek <marcel.moravek@yahoo.de>
2025-02-06 10:06:37 +0100

    docs: update tooltip docs

--- packages/oruga/src/components/tooltip/examples/index.vue
+++ packages/oruga/src/components/tooltip/examples/index.vue
@@ -19,21 +19,47 @@ import TriggersCode from "./triggers.vue?raw";
 </script>
 
 <template>
-    <h3 id="base">Base</h3>
-    <ExampleViewer :component="Base" :code="BaseCode" />
+    <div class="vp-doc">
+        <h3 id="base">Base</h3>
+        <ExampleViewer :component="Base" :code="BaseCode" />
 
-    <h3 id="position">Position</h3>
-    <ExampleViewer :component="Position" :code="PositionCode" />
+        <h3 id="position">Position</h3>
+        <p>
+            The direction in which the tooltip opens can be defined by the
+            <code>position</code> prop. By default, the direction is
+            automatically calculated from the distance to the edge of the
+            window. Adding the <code>teleport</code> prop additionally will move
+            the tooltip to the referenced DOM location instead.
+        </p>
+        <ExampleViewer :component="Position" :code="PositionCode" />
 
-    <h3 id="variants">Variants</h3>
-    <ExampleViewer :component="Variants" :code="VariantsCode" />
+        <h3 id="triggers">Triggers</h3>
+        <p>
+            The action to trigger the tooltip can be customized using the
+            <code>triggers</code> prop. The action that closes the tooltip can
+            be customized using the <code>closaeble</code> prop.
+        </p>
 
-    <h3 id="multiline">Multiline</h3>
-    <ExampleViewer :component="Multiline" :code="MultilineCode" />
+        <ExampleViewer :component="Triggers" :code="TriggersCode" />
 
-    <h3 id="slot">Slot</h3>
-    <ExampleViewer :component="Slot" :code="SlotCode" />
+        <h3 id="variants">Variants</h3>
+        <p>
+            Different styles can be achieved with the <code>variant</code> prop.
+        </p>
+        <ExampleViewer :component="Variants" :code="VariantsCode" />
 
-    <h3 id="triggers">Triggers</h3>
-    <ExampleViewer :component="Triggers" :code="TriggersCode" />
+        <h3 id="multiline">Multiline</h3>
+        <p>
+            Sometimes the tooltip label can be very long. Consider setting the
+            <code>multiline</code> prop to force a line break.
+        </p>
+        <ExampleViewer :component="Multiline" :code="MultilineCode" />
+
+        <h3 id="slot">Slot</h3>
+        <p>
+            The tooltip label can be customised using the
+            <code>content</code> slot.
+        </p>
+        <ExampleViewer :component="Slot" :code="SlotCode" />
+    </div>
 </template>
--- packages/oruga/src/components/tooltip/examples/triggers.vue
+++ packages/oruga/src/components/tooltip/examples/triggers.vue
@@ -6,40 +6,42 @@ const active = ref(true);
 
 <template>
     <section class="odocs-spaced">
-        <o-field>
-            <o-switch v-model="active" label="Toggle" />
-        </o-field>
+        <p>
+            <o-field>
+                <o-switch v-model="active" label="Toggle" />
+            </o-field>
 
-        <o-tooltip
-            label="I'm never closing"
-            :triggers="['click']"
-            :closeable="['outside']">
-            <o-button label="Click me" />
-        </o-tooltip>
+            <o-tooltip
+                label="I'm never closing"
+                :triggers="['click']"
+                :closeable="['outside']">
+                <o-button label="Click me" />
+            </o-tooltip>
 
-        <o-tooltip
-            label="I'm never closing"
-            :triggers="['contextmenu']"
-            :closeable="['outside']">
-            <o-button label="Right click me" />
-        </o-tooltip>
+            <o-tooltip
+                label="I'm never closing"
+                :triggers="['contextmenu']"
+                :closeable="['outside']">
+                <o-button label="Right click me" />
+            </o-tooltip>
 
-        <o-tooltip
-            label="I'm never closing"
-            :active="active"
-            always
-            position="bottom">
-            <o-button label="Always" />
-        </o-tooltip>
+            <o-tooltip
+                label="I'm never closing"
+                :active="active"
+                always
+                position="top">
+                <o-button label="Always" />
+            </o-tooltip>
 
-        <o-tooltip
-            variant="danger"
-            label="Tooltip right"
-            position="right"
-            :triggers="[]"
-            :closeable="false"
-            :active="active">
-            <o-button label="Toggled" />
-        </o-tooltip>
+            <o-tooltip
+                variant="danger"
+                label="Tooltip right"
+                position="right"
+                :triggers="[]"
+                :closeable="false"
+                :active="active">
+                <o-button label="Toggled" />
+            </o-tooltip>
+        </p>
     </section>
 </template>
