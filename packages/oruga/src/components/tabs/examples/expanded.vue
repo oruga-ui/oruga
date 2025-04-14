4c4202e461566e5b717b2abd5c930dd82b992e24
Ben Webb <ben.webb@whiplash.com>
2025-04-14 11:14:09 -0400

    docs : add tab examples

--- packages/oruga/src/components/tabs/examples/expanded.vue
+++ packages/oruga/src/components/tabs/examples/expanded.vue
@@ -3,7 +3,10 @@
         <o-tabs expanded>
             <o-tab-item label="Pictures" icon="images" />
             <o-tab-item label="Music" icon="music" />
+            <o-tab-item label="Words" />
             <o-tab-item label="Videos" icon="video" />
         </o-tabs>
     </section>
 </template>
+<script setup lang="ts">
+</script>
--- packages/oruga/src/components/tabs/examples/position.vue
+++ packages/oruga/src/components/tabs/examples/position.vue
@@ -3,19 +3,24 @@
         <o-tabs type="boxed" position="left">
             <o-tab-item label="Pictures" icon="images" />
             <o-tab-item label="Music" icon="music" />
+            <o-tab-item label="Words" />
             <o-tab-item label="Videos" icon="video" />
         </o-tabs>
 
         <o-tabs type="boxed" position="centered">
             <o-tab-item label="Pictures" icon="images" />
             <o-tab-item label="Music" icon="music" />
+            <o-tab-item label="Words" />
             <o-tab-item label="Videos" icon="video" />
         </o-tabs>
 
         <o-tabs type="boxed" position="right">
             <o-tab-item label="Pictures" icon="images" />
             <o-tab-item label="Music" icon="music" />
+            <o-tab-item label="Words" />
             <o-tab-item label="Videos" icon="video" />
         </o-tabs>
     </section>
 </template>
+<script setup lang="ts">
+</script>
