511e6a0df9423fdd25746ec9f9b60c8764d3f13c
Mouad Taoussi <50294614+mouadTaoussi@users.noreply.github.com>
2025-04-15 21:49:16 +0200

    writing test

--- packages/oruga/src/components/breadcrumb/tests/breadcrumbitem.test.ts
+++ packages/oruga/src/components/breadcrumb/tests/breadcrumbitem.test.ts
@@ -6,34 +6,56 @@ import OBreadcrumb from "@/components/breadcrumb/Breadcrumb.vue";
 import OBreadcrumbItem from "../BreadcrumbItem.vue";
 
 describe("<BreadcrumbItem>", () => {
+    const component = {
+        components: { OBreadcrumb, OBreadcrumbItem },
+        props: ["options"],
+        template: `
+            <o-breadcrumb tag="section" align="centered">
+                <o-breadcrumb-item active="true" tag="a" href="/" active-variant="primary"
+                    >Home
+                </o-breadcrumb-item>
+                <o-breadcrumb-item tag="a" href="/documentation/" active-variant="primary" disabled="true"
+                    >Docs
+                </o-breadcrumb-item>
+                <o-breadcrumb-item tag="a" href="/components/Breadcrumb" active-variant="primary"
+                    >Breadcrumb
+                </o-breadcrumb-item>
+            </o-breadcrumb>
+        `,
+    };
+
     test("is active ", async () => {
-        const component = {
-            components: { OBreadcrumb, OBreadcrumbItem },
-            props: ["options"],
-            template: `
-                <o-breadcrumb tag="section" align="centered">
-                    <o-breadcrumb-item active tag="router-link" to="/" active-variant="primary"
-                        >Home
-                    </o-breadcrumb-item>
-                    <o-breadcrumb-item tag="router-link" to="/documentation/" active-variant="primary" disabled="true"
-                        >Docs
-                    </o-breadcrumb-item>
-                    <o-breadcrumb-item tag="router-link" to="/components/Breadcrumb" active-variant="primary"
-                        >Breadcrumb
-                    </o-breadcrumb-item>
-                </o-breadcrumb>
-            `,
-        };
         const wrapper = mount(component);
 
         await nextTick(); // await breadcrumb item rendered
 
         const items = wrapper.findAllComponents(OBreadcrumbItem);
-        // expect(items[0].classes("o-breadcrumb-item__primary")).toBeTruthy();
-        // expect(items[0].classes("primary")).toBeTruthy();
-        expect(items[0].attributes("active-variant")).toBe("primary");
+
+        expect(items[0].classes("o-breadcrumb-item__primary")).toBeTruthy();
+        expect(items[0].classes("active")).toBeTruthy();
     });
-    test("is disabled", () => {});
-    test("has icon", () => {});
-    test("has href link", () => {});
+    test("is disabled", async () => {
+        const wrapper = mount(component);
+
+        await nextTick(); // await breadcrumb item rendered
+
+        const items = wrapper.findAllComponents(OBreadcrumbItem);
+        expect(true).toBe(true);
+    });
+    test("has icon", async () => {
+        const wrapper = mount(component);
+
+        await nextTick(); // await breadcrumb item rendered
+
+        const items = wrapper.findAllComponents(OBreadcrumbItem);
+        expect(true).toBe(true);
+    });
+    test("has href link", async () => {
+        const wrapper = mount(component);
+
+        await nextTick(); // await breadcrumb item rendered
+
+        const items = wrapper.findAllComponents(OBreadcrumbItem);
+        expect(true).toBe(true);
+    });
 });
