d4be6bf67b0e65b189e25b9b92594a1ca9e44918
Ben Webb <119433664+whiplashwebb@users.noreply.github.com>
2025-04-28 12:05:29 -0400

    docs(tabs): enhance examples (#1280)

--- packages/oruga/src/components/tabs/examples/position.vue
+++ packages/oruga/src/components/tabs/examples/position.vue
@@ -3,18 +3,21 @@
         <o-tabs type="boxed" position="left">
             <o-tab-item label="Pictures" icon="images" />
             <o-tab-item label="Music" icon="music" />
+            <o-tab-item label="Words" />
             <o-tab-item label="Videos" icon="video" />
         </o-tabs>
 
         <o-tabs type="boxed" position="centered">
             <o-tab-item label="Pictures" icon="images" />
             <o-tab-item label="Music" icon="music" />
+            <o-tab-item label="Words" />
             <o-tab-item label="Videos" icon="video" />
         </o-tabs>
 
         <o-tabs type="boxed" position="right">
             <o-tab-item label="Pictures" icon="images" />
             <o-tab-item label="Music" icon="music" />
+            <o-tab-item label="Words" />
             <o-tab-item label="Videos" icon="video" />
         </o-tabs>
     </section>
