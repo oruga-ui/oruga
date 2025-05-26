3882aab5c42efbbef5d1f93cd76bed9e950da9dd
mm <25961416+mlmoravek@users.noreply.github.com>
2025-05-26 15:06:36 +0200

    fix(table): refilter rows when `paginated` prop change (#1316)

--- packages/oruga/src/components/table/Table.vue
+++ packages/oruga/src/components/table/Table.vue
@@ -427,8 +427,14 @@ const isScrollable = computed(() => {
 const tableCurrentPage = defineModel<number>("currentPage", { default: 1 });
 
 // recompute table rows visibility on page change or data change
-watch([tableCurrentPage, () => props.perPage, () => props.data], () =>
-    filterTableRows(),
+watch(
+    [
+        tableCurrentPage,
+        () => props.perPage,
+        () => props.data,
+        () => props.paginated,
+    ],
+    () => filterTableRows(),
 );
 
 // create a unique id sequence
@@ -469,7 +475,7 @@ function filterTableRows(): void {
     // update hidden state for each row
     filterOptionsItems(tableRows, (row, idx) => {
         // if paginated not backend paginated, paginate row
-        if (props.paginated || !props.backendPagination) {
+        if (props.paginated && !props.backendPagination) {
             // if not only one page and not on active page
             if (
                 tableRows.value.length > perPage &&
