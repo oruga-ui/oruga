448557c314107e4343939327bb302db57b56f10a
mmoravek <marcel.moravek@yahoo.de>
2025-06-09 11:15:35 +0200

    refactor: add `attrs` binding to PlainButton

--- packages/oruga/src/components/utils/PlainButton.ts
+++ packages/oruga/src/components/utils/PlainButton.ts
@@ -5,12 +5,13 @@ import { createVNode, type FunctionalComponent } from "vue";
  *
  * Useful as a default tag for components that use custom action buttons
  */
-const PlainButton: FunctionalComponent = (props, { slots }) =>
+const PlainButton: FunctionalComponent = (props, { slots, attrs }) =>
     createVNode(
         "button",
         {
             type: "button",
             ...props,
+            ...attrs,
         },
         slots.default?.(),
     );
