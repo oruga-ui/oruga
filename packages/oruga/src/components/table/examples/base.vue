79d16dcdab9d617958d36b17697cd6e3c2e4c412
mlmoravek <marcel.moravek@yahoo.de>
2025-06-26 15:58:36 +0200

    docs: revert table base example

--- packages/oruga/src/components/table/examples/base.vue
+++ packages/oruga/src/components/table/examples/base.vue
@@ -1,69 +1,57 @@
 <script setup lang="ts">
-/****************************************
- * THIS CHANGE IS ONLY FOR TEST PURPOSE
- ******************************************/
-import { OButton, OTable, OTableColumn } from "@oruga-ui/oruga-next";
 import { ref } from "vue";
 
-const data = ref([]);
-
-const random_words = [
-    "apple",
-    "beach",
-    "cloud",
-    "dance",
-    "eagle",
-    "flame",
-    "grace",
-    "heart",
-    "image",
-    "jolly",
-    "knife",
-    "light",
-    "music",
-    "noble",
-    "ocean",
-    "peace",
-    "quick",
-    "river",
-    "storm",
-    "table",
-    "under",
-    "voice",
-    "water",
-    "youth",
-    "zebra",
-    "amber",
-    "blend",
-    "craft",
-    "dream",
-    "earth",
-    "frost",
-    "green",
-];
-
-let lastId = 1;
-
-function add(): void {
-    // @ts-ignore
-    data.value.push({
-        id: lastId,
-        name: random_words[Math.floor(Math.random() * random_words.length)],
-    });
-    lastId++;
-}
-
-function replace(): void {
-    const data_copy = data.value.slice();
-    data.value = data_copy;
-}
+const data = ref([
+    {
+        id: 1,
+        first_name: "Jesse",
+        last_name: "Simmons",
+        date: "2016-10-15 13:43:27",
+        gender: "Male",
+    },
+    {
+        id: 2,
+        first_name: "John",
+        last_name: "Jacobs",
+        date: "2016-12-15 06:00:53",
+        gender: "Male",
+    },
+    {
+        id: 3,
+        first_name: "Tina",
+        last_name: "Gilbert",
+        date: "2016-04-26 06:26:28",
+        gender: "Female",
+    },
+    {
+        id: 4,
+        first_name: "Clarence",
+        last_name: "Flores",
+        date: "2016-04-10 10:28:46",
+        gender: "Male",
+    },
+    {
+        id: 5,
+        first_name: "Anne",
+        last_name: "Lee",
+        date: "2016-12-06 14:38:38",
+        gender: "Female",
+    },
+]);
 </script>
 
 <template>
-    <OButton @click="add">Add record</OButton>
-    <OButton @click="replace">Replace recordset</OButton>
-
-    <OTable :data="data" paginated :per-page="5" default-sort="name">
-        <OTableColumn label="Name" field="name" sortable />
-    </OTable>
+    <section>
+        <o-table :data="data">
+            <o-table-column field="id" label="Id" width="40" numeric sortable />
+            <o-table-column field="first_name" label="First Name" sortable />
+            <o-table-column field="last_name" label="Last Name" sortable />
+            <o-table-column
+                field="date"
+                label="Date"
+                position="centered"
+                sortable />
+            <o-table-column field="gender" label="Gender" sortable />
+        </o-table>
+    </section>
 </template>
