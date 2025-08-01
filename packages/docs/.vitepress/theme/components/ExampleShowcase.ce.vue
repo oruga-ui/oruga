f9ffe73e13b44b2673d467bde29655abc2bfd401
mlmoravek <marcel.moravek@yahoo.de>
2025-08-01 15:25:33 +0200

    feat(listbox): add listbox component

--- packages/docs/.vitepress/theme/components/ExampleShowcase.ce.vue
+++ packages/docs/.vitepress/theme/components/ExampleShowcase.ce.vue
@@ -18,7 +18,7 @@ if (!shadowRoot)
 onMounted(() => {
     if (host?.children)
         // add bulma light theme attribute to the slot content
-        host.childNodes.forEach((child) =>
+        Array.from(host.children).forEach((child) =>
             child.setAttribute("data-theme", "light"),
         );
 
@@ -49,7 +49,7 @@ shadowRoot.appendChild(link);
 <!-- eslint-disable-next-line vue/valid-template-root -->
 <template></template>
 
-<style>
+<style lang="scss">
 /** class of the inpected element in the Class Inpector */
 .odocs-inspected-element {
     border: 2px solid var(--vp-c-caution-1) !important;
@@ -63,5 +63,109 @@ shadowRoot.appendChild(link);
 
 .odocs-spaced p > *:not(:last-child) {
     margin-right: 0.5rem;
+}
+
+.o-listbox {
+    display: block;
+    background: var(--p-listbox-background);
+    color: var(--p-listbox-color);
+    border: 1px solid var(--p-listbox-border-color);
+    border-radius: var(--p-listbox-border-radius);
+    transition:
+        background var(--p-listbox-transition-duration),
+        color var(--p-listbox-transition-duration),
+        border-color var(--p-listbox-transition-duration),
+        box-shadow var(--p-listbox-transition-duration),
+        outline-color var(--p-listbox-transition-duration);
+    outline-color: transparent;
+    box-shadow: var(--p-listbox-shadow);
+    background-color: var(--oruga-dropdown-menu-background, #ffffff);
+
+    border-radius: var(
+        --oruga-dropdown-menu-border-radius,
+        var(--oruga-base-border-radius)
+    );
+    box-shadow: var(
+        --oruga-dropdown-menu-box-shadow,
+        0 0.5em 1em -0.125em rgba(10, 10, 10, 0.1),
+        0 0 0 1px rgba(10, 10, 10, 0.02)
+    );
+    padding: var(--oruga-dropdown-menu-padding, 0.5rem 0 0.5rem 0);
+    margin: var(--oruga-dropdown-menu-margin, 0);
+
+    &__filter {
+        padding: var(--p-listbox-list-header-padding);
+    }
+
+    &__container {
+        display: block;
+        overflow: auto;
+        min-width: var(--oruga-dropdown-menu-width, 12rem);
+        z-index: var(--oruga-dropdown-menu-zindex, 50);
+    }
+
+    &__list {
+        list-style-type: none;
+        margin: 0;
+        padding: var(--p-listbox-list-padding);
+        outline: 0 none;
+        display: flex;
+        flex-direction: column;
+        gap: var(--p-listbox-list-gap);
+
+        &:focus-visible {
+            border: 1px solid red;
+        }
+    }
+
+    &__item {
+        display: block;
+        position: relative;
+        color: var(--oruga-dropdown-item-color, #000000);
+        font-size: var(
+            --oruga-dropdown-item-font-size,
+            var(--oruga-base-font-size)
+        );
+        font-weight: var(--oruga-dropdown-item-font-weight, 400);
+        line-height: var(
+            --oruga-dropdown-item-line-height,
+            var(--oruga-base-line-height)
+        );
+        padding: var(--oruga-dropdown-item-padding, 0.375rem 1rem);
+
+        &--disabled {
+            opacity: var(
+                --oruga-dropdown-item-disabled-opacity,
+                var(--oruga-base-disabled-opacity)
+            );
+            pointer-events: none;
+        }
+
+        &--selected {
+            background-color: var(
+                --oruga-dropdown-item-active-background-color,
+                var(--oruga-primary)
+            );
+            color: var(
+                --oruga-dropdown-item-active-color,
+                var(--oruga-primary-invert)
+            );
+        }
+
+        &:not(.o-listbox__item--disabled) {
+            cursor: pointer;
+
+            &:not(.o-listbox__item--selected) {
+                &:hover,
+                &.o-listbox__item--focused {
+                    background-color: var(
+                        --oruga-dropdown-item-hover-background-color,
+                        #f5f5f5
+                    );
+                    color: var(--oruga-dropdown-item-hover-color, #000000);
+                }
+            }
+        }
+    }
 }
 </style>
