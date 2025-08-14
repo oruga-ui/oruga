8779f35c5ae3e63c91da934fa6f1ea1adfb8bf6b
mlmoravek <marcel.moravek@yahoo.de>
2025-08-14 10:00:04 +0200

    refactor: remove `isSSR` utility

--- packages/oruga/src/utils/ssr.ts
+++ packages/oruga/src/utils/ssr.ts
@@ -1,7 +1,6 @@
 // Polyfills for SSR
 
 export const isClient = typeof window !== "undefined";
-export const isSSR = typeof window === "undefined";
 
 export const HTMLElement = isClient ? window.HTMLElement : Object;
 export const HTMLInputElement = isClient ? window.HTMLInputElement : Object;
