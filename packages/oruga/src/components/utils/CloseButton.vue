6ef662c811c3b0a8a5be468dc253eed2c0668b6c
mlmoravek <marcel.moravek@yahoo.de>
2025-08-29 10:22:33 +0200

    refactor: add internal CloseButton utility component

--- packages/oruga/src/components/utils/CloseButton.vue
+++ packages/oruga/src/components/utils/CloseButton.vue
@@ -2,17 +2,23 @@
 import type { PropType } from "vue";
 import type { ClassBind } from "@/types";
 
+import OIcon from "@/components/icon/Icon.vue";
+
 defineProps({
+    label: { type: String, required: true },
     icon: { type: String, required: true },
     size: { type: String, default: undefined },
-    pack: { type: String, required: true },
-    label: { type: String, required: true },
+    pack: { type: String, default: undefined },
     classes: { type: Array as PropType<ClassBind[]>, required: true },
 });
 </script>
 
 <template>
-    <button type="button" :class="classes" :aria-label="label">
+    <button
+        type="button"
+        :class="classes"
+        :aria-label="label"
+        data-oruga="close">
         <o-icon :pack="pack" :icon="icon" :size="size" />
     </button>
 </template>
