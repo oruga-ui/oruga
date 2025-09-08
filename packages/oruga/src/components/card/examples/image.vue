2222beef91ee33ca57d2cb69ea05038b3738f851
mm <25961416+mlmoravek@users.noreply.github.com>
2025-09-08 20:15:49 +0200

    feat(card): add OCard component (#1384)

---
+++ packages/oruga/src/components/card/examples/image.vue
@@ -0,0 +1,7 @@
+<template>
+    <o-card
+        title="Advanced Card"
+        content="Some quick example text to build on the card title and make up the bulk of the card's content."
+        image-alt="Oruga Logo"
+        image-src="https://avatars2.githubusercontent.com/u/66300512?s=200&v=4" />
+</template>
