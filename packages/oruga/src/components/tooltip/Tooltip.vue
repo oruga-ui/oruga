f60fee1da4ee834a18fb22a96e6a1a68417b09b6
mm <25961416+mlmoravek@users.noreply.github.com>
2025-11-22 12:54:50 +0100

    fix(tooltip): remove incorrectly used `aria-haspopup` attribute (#1476)
    - remove incorrectly used `aria-haspopup` from tooltip
    - update tooltip documenation for a11y information

--- packages/oruga/src/components/tooltip/Tooltip.vue
+++ packages/oruga/src/components/tooltip/Tooltip.vue
@@ -249,7 +249,6 @@ const arrowClasses = defineClasses(
             :is="triggerTag"
             ref="triggerRef"
             :class="triggerClasses"
-            aria-haspopup="true"
             :aria-describedby="tooltipId"
             @click="onClick"
             @contextmenu="onContextMenu"
