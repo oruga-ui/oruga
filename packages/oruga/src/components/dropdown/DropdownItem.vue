81ff91aff2e07a2a885a3c823ea0dce8951dd79d
mlmoravek <marcel.moravek@yahoo.de>
2026-06-11 17:06:38 +0200

    chore: resolve eslint issues

--- packages/oruga/src/components/dropdown/DropdownItem.vue
+++ packages/oruga/src/components/dropdown/DropdownItem.vue
@@ -46,7 +46,6 @@ const rootRef = useTemplateRef<HTMLElement>("rootElement");
 
 // provided data is a computed ref to ensure reactivity
 const providedData = computed<DropdownItemComponent<T>>(() => ({
-    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     value: props.value as T,
     label: props.label,
     isViable: isViable.value,
