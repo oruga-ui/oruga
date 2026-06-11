26cece93c12bed8531f98da656df9321b608deba
mlmoravek <marcel.moravek@yahoo.de>
2026-06-11 16:01:24 +0200

    test(datepicker): update tests

---
+++ packages/oruga/src/components/datepicker/tests/datepicker.axe.test.ts
@@ -0,0 +1,48 @@
+import { defineComponent, h } from "vue";
+import { afterEach, describe, expect, test } from "vitest";
+import { enableAutoUnmount, mount } from "@vue/test-utils";
+import { axe } from "jest-axe";
+import { setTimeout } from "timers/promises";
+
+import ODatepicker from "../Datepicker.vue";
+import type { DatepickerProps } from "../props";
+import OField from "../../field/Field.vue";
+
+const BrowserDropdown = defineComponent(
+    (props) => () =>
+        h(
+            OField,
+            { label: "My Input" },
+            {
+                default: () => h(ODatepicker, props),
+            },
+        ),
+);
+
+describe("ODatepicker axe tests", () => {
+    enableAutoUnmount(afterEach);
+
+    const a11yCases: {
+        title: string;
+        props?: DatepickerProps<true | false, true | false>;
+    }[] = [
+        {
+            title: "axe datepicker - base case",
+            props: {},
+        },
+        {
+            title: "axe datepicker - open popover",
+            props: { active: true },
+        },
+    ];
+
+    test.each(a11yCases)("$title", async ({ props }) => {
+        const wrapper = mount(BrowserDropdown, {
+            props: { ...props },
+            attachTo: document.body,
+        });
+        await setTimeout(); // await child items got rendered
+
+        expect(await axe(wrapper.element)).toHaveNoViolations();
+    });
+});
