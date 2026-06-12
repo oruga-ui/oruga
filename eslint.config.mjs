3dd4cd466197699d17fb2e0fec1ea5a050abfa27
mm <25961416+mlmoravek@users.noreply.github.com>
2026-06-12 16:47:04 +0200

    chore: add eslint compat for browser compatibility check (#1649)
    - add [`eslint-plugin-compat`](https://github.com/amilajack/eslint-plugin-compat) lint rules to check browser compatability
    - add browserslist [`defaults`](https://github.com/browserslist/browserslist#full-list) without `op_mini` config to package.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
 import eslint from "@eslint/js";
 import { globalIgnores } from "eslint/config";
 import { includeIgnoreFile } from "@eslint/compat";
+import compat from "eslint-plugin-compat";
 import vuePlugin from "eslint-plugin-vue";
 import {
   configureVueProject,
@@ -35,6 +36,9 @@ export default defineConfigWithVueTs([
 
   // include .gitignore ignore patterns
   includeIgnoreFile(gitignorePath),
+
+  // add browser compatibility configs
+  compat.configs["flat/recommended"],
 
   // add js configs
   eslint.configs.recommended,
