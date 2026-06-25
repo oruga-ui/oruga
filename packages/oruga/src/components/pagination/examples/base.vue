2040f8db2ea87dcab6abd84788bfe06763e13ea1
mlmoravek <marcel.moravek@yahoo.de>
2026-06-25 11:11:02 +0200

    feat(pagination): rename `current` to `modelValue`

--- packages/oruga/src/components/pagination/examples/base.vue
+++ packages/oruga/src/components/pagination/examples/base.vue
@@ -1,13 +1,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const current = ref(2);
+const page = ref(2);
 </script>
 
 <template>
     <section>
-        <o-pagination v-model:current="current" :total="100" :per-page="10" />
+        <o-pagination v-model="page" :total="100" :per-page="10" />
 
-        <p><b>Current:</b> {{ current }}</p>
+        <p><b>Current:</b> {{ page }}</p>
     </section>
 </template>
