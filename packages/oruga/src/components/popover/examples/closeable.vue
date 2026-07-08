edc6855d1cdaeb3370ee26c5a39bfad2997df273
mmoravek <marcel.moravek@yahoo.de>
2026-07-08 19:29:04 +0200

    docs: add example

---
+++ packages/oruga/src/components/popover/examples/closeable.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+        <o-popover closeable behavior="manual">
+            <template #default="{ open }">
+                <o-button @click="open"> Open popover! </o-button>
+            </template>
+
+            <template #content>
+                <b>Lorem ipsum dolor sit amet</b>, consectetur warning elit.
+                <i>Fusce id fermentum quam</i>.
+                <o-icon icon="heart" variant="danger" />
+            </template>
+        </o-popover>
+    </section>
+</template>
