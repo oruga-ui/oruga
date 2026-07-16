902ddb87fa17e1b9793926ec17d141cd26cbc927
mlmoravek <marcel.moravek@yahoo.de>
2026-07-16 11:55:23 +0200

    feat(slider): add a `tickBeforeClass` and `tickAfterClass` class

--- packages/oruga/src/components/slider/SliderTick.vue
+++ packages/oruga/src/components/slider/SliderTick.vue
@@ -59,9 +59,7 @@ const hidden = computed(
 const tickStyle = computed(() => ({ left: position.value + "%" }));
 
 const isBefore = computed(() => parent.value.valueStart > props.value);
-const isAfter = computed(
-    () => (parent.value.valueEnd ?? parent.value.valueStart) < props.value,
-);
+const isAfter = computed(() => parent.value.valueEnd < props.value);
 
 // #region --- Computed Component Classes ---
 
