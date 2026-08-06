e1adc8640a5c2b570702dbb13a3e30ec896b184b
mlmoravek <marcel.moravek@yahoo.de>
2026-08-06 09:47:07 +0200

    test: update tests

--- packages/oruga/src/components/icon/tests/icon.unit.test.ts
+++ packages/oruga/src/components/icon/tests/icon.unit.test.ts
@@ -13,4 +13,20 @@ describe("OIcon tests", () => {
         expect(wrapper.attributes("data-oruga")).toBe("icon");
         expect(wrapper.html()).toMatchSnapshot();
     });
+
+    test("sets aria-hidden when no aria label is provided", () => {
+        const wrapper = mount(OIcon);
+
+        expect(wrapper.attributes("aria-hidden")).toBe("true");
+    });
+
+    test("does not set aria-hidden when an aria label is provided", () => {
+        const wrapper = mount(OIcon, {
+            props: {
+                ariaLabel: "Close",
+            },
+        });
+
+        expect(wrapper.attributes("aria-hidden")).toBeUndefined();
+    });
 });
